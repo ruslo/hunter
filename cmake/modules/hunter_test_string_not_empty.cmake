6d256e8ae88496fbc5ae7610adef88e0a84aa9db
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-11-27 18:24:03 +0400

    Add function: hunter_test_string_not_empty [skip ci]

---
+++ cmake/modules/hunter_test_string_not_empty.cmake
@@ -0,0 +1,10 @@
+# Copyright (c) 2013, Ruslan Baratov
+# All rights reserved.
+
+include(hunter_fatal_error)
+
+function(hunter_test_string_not_empty test_string)
+  if(NOT test_string)
+    hunter_fatal_error("Unexpected empty string")
+  endif()
+endif()
