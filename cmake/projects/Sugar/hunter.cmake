2276d6e2c0bd3ce1de0b28d07549fab20aeef778
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-12-08 22:31:33 +0400

    Update sugar: 1.0.2

--- cmake/projects/Sugar/hunter.cmake
+++ cmake/projects/Sugar/hunter.cmake
@@ -35,6 +35,17 @@ hunter_add_version(
     1a8e29badc0e97913996e5228dc9b1788284e416
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    Sugar
+    VERSION
+    "1.0.2"
+    URL
+    "https://github.com/ruslo/sugar/archive/v1.0.2.tar.gz"
+    SHA1
+    393a2c190f76f52b253089d0a03c08de562a8622
+)
+
 hunter_download(
     PACKAGE_NAME
     Sugar
