6f016155e244279695acc308baed5646297be8c1
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-12-09 14:28:24 +0400

    Update gtest: 1.7.0-hunter-1

--- cmake/projects/GTest/hunter.cmake
+++ cmake/projects/GTest/hunter.cmake
@@ -28,6 +28,17 @@ hunter_add_version(
     1ed1c26d11fb592056c1cb912bd3c784afa96eaa
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    GTest
+    VERSION
+    "1.7.0-hunter-1"
+    URL
+    "https://github.com/hunter-packages/gtest/archive/v1.7.0-hunter-1.tar.gz"
+    SHA1
+    0cb1dcf75e144ad052d3f1e4923a7773bf9b494f
+)
+
 hunter_download(
     PACKAGE_NAME
     GTest
