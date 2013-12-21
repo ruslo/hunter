f47ab8f430ffd1aa836e6f4576c285ddccb608b9
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-12-21 22:41:49 +0400

    Add ios_sim package

---
+++ cmake/projects/ios_sim/hunter.cmake
@@ -0,0 +1,32 @@
+# Copyright (c) 2013, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED HUNTER_CMAKE_PROJECTS_IOS_SIM_HUNTER_CMAKE)
+  return()
+else()
+  set(HUNTER_CMAKE_PROJECTS_IOS_SIM_HUNTER_CMAKE 1)
+endif()
+
+include(hunter_add_version)
+include(hunter_add_version_start)
+include(hunter_download)
+
+hunter_add_version_start(ios_sim)
+
+hunter_add_version(
+    PACKAGE_NAME
+    ios_sim
+    VERSION
+    "1.8.2"
+    URL
+    "https://github.com/phonegap/ios-sim/archive/1.8.2.tar.gz"
+    SHA1
+    4328b3c8e6b455631d52b7ce5968170c9769eb1e
+)
+
+hunter_download(
+    PACKAGE_NAME
+    ios_sim
+    DOWNLOAD_SCHEME
+    url_sha1_ios_sim
+)
