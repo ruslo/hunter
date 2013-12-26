3f2d7317c616db225b1b61e426be6cf741bfccbe
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-12-26 20:14:18 +0400

    Add boost component generator

---
+++ cmake/projects/Boost/generate.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+# Copyright (c) 2013, Ruslan Baratov
+# All rights reserved.
+
+BOOST_LIBS="
+    atomic
+    chrono
+    context
+    coroutine
+    date_time
+    exception
+    filesystem
+    graph
+    graph_parallel
+    iostreams
+    locale
+    log
+    math
+    mpi
+    program_options
+    python
+    random
+    regex
+    serialization
+    signals
+    system
+    test
+    thread
+    timer
+    wave
+"
+
+for library in ${BOOST_LIBS};
+do
+  LIBRARY_UPPER=`echo ${library} | tr '[:lower:]' '[:upper:]'`
+  echo "${library} -- ${LIBRARY_UPPER}"
+
+  mkdir -p ${library}
+  cp hunter.cmake.in ${library}/hunter.cmake
+  sed -i.back "s,boost_component,${library}," ${library}/hunter.cmake
+  sed -i.back "s,BOOST_COMPONENT,${LIBRARY_UPPER}," ${library}/hunter.cmake
+done
