91bbb053167a7280a98a1fc67f0d29afe91165e1
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-01-03 21:47:13 +0400

    Add function: hunter_set_cmake_default_generator

---
+++ cmake/modules/hunter_set_cmake_default_generator.cmake
@@ -0,0 +1,56 @@
+# Copyright (c) 2014, Ruslan Baratov
+# All rights reserved.
+
+include(hunter_fatal_error)
+include(hunter_test_string_not_empty)
+
+function(hunter_set_cmake_default_generator)
+  hunter_test_string_not_empty("${HUNTER_BASE}")
+  hunter_test_string_not_empty("${HUNTER_INSTALL_TAG}")
+  hunter_test_string_not_empty("${PROJECT_BINARY_DIR}")
+
+  set(
+      generator_info
+      "${HUNTER_BASE}/Toolchains/${HUNTER_INSTALL_TAG}/default_generator.info"
+  )
+
+  if(EXISTS "${generator_info}")
+    return()
+  endif()
+
+  hunter_status_debug("File `${generator_info}` not found, run test...")
+
+  set(temp_project_dir "${PROJECT_BINARY_DIR}/_Hunter-temp-REMOVE_ME")
+  set(temp_build_dir "${temp_project_dir}/_builds")
+
+  file(
+      WRITE
+      "${temp_project_dir}/CMakeLists.txt"
+      "cmake_minimum_required(VERSION 2.8)\n"
+      "project(temp)\n"
+      "file(\n"
+      "    WRITE\n"
+      "    \"${generator_info}\"\n"
+      "    \"\${CMAKE_GENERATOR}\"\n"
+      ")\n"
+  )
+
+  execute_process(
+      COMMAND
+      "${CMAKE_COMMAND}"
+      "-H${temp_project_dir}"
+      "-B${temp_build_dir}"
+      RESULT_VARIABLE
+      generate_result
+  )
+
+  if(NOT generate_result EQUAL "0")
+    message(FATAL_ERROR "Generate failed: exit with code ${generate_result}")
+  endif()
+
+  if(NOT EXISTS "${generator_info}")
+    hunter_fatal_error("Internal error `${generator_info}` not exists")
+  endif()
+
+  file(REMOVE_RECURSE "${temp_project_dir}")
+endfunction()
