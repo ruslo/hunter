3996e468e5004c35434562a418a8bf8f68c0d899
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-01-03 21:33:58 +0400

    Improve hunter_test_string_not_empty

--- cmake/modules/hunter_test_string_not_empty.cmake
+++ cmake/modules/hunter_test_string_not_empty.cmake
@@ -4,7 +4,8 @@
 include(hunter_fatal_error)
 
 function(hunter_test_string_not_empty test_string)
-  if(NOT test_string)
+  string(COMPARE EQUAL "${test_string}" "" is_empty)
+  if(is_empty)
     hunter_fatal_error("Unexpected empty string")
   endif()
 endfunction()
