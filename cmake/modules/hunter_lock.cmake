970c01bb1950ca87cd53a09115e42f0d5c741c18
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-01-15 13:46:17 +0400

    Remove lock if build directory that made lock not exists

--- cmake/modules/hunter_lock.cmake
+++ cmake/modules/hunter_lock.cmake
@@ -22,20 +22,29 @@ function(hunter_lock)
   hunter_status_debug("lock directory")
 
   set(lock_file "${HUNTER_ROOT}/Base/hunter-build.lock")
+  set(binary_dir_file "${HUNTER_ROOT}/Base/binary-dir.lock")
+
   if(EXISTS "${lock_file}")
-    file(READ "${lock_file}" build_info)
-    message(
-      "\n"
-      "Another project is using directory right now:\n\n"
-      "    ${HUNTER_ROOT}\n\n"
-      "Build info: \n\n"
-      "${build_info}\n\n"
-      "If that build cancelled (interrupted by user or some other reason), "
-      "please remove this file manually:\n\n"
-      "    ${lock_file}\n\n"
-      "Then run CMake again."
-    )
-    hunter_fatal_error("Directory busy")
+    if(NOT EXISTS "${binary_dir_file}")
+      hunter_fatal_error("Lock file exists but `${binary_dir_file}` not found")
+    endif()
+    file(READ "${binary_dir_file}" locked_binary_dir)
+    if(EXISTS "${locked_binary_dir}")
+      # directory still exists and may be active
+      file(READ "${lock_file}" build_info)
+      message(
+        "\n"
+        "Another project is using directory right now:\n\n"
+        "    ${HUNTER_ROOT}\n\n"
+        "Build info: \n\n"
+        "${build_info}\n\n"
+        "If that build cancelled (interrupted by user or some other reason), "
+        "please remove this file manually:\n\n"
+        "    ${lock_file}\n\n"
+        "Then run CMake again."
+      )
+      hunter_fatal_error("Directory busy")
+    endif()
   endif()
 
   string(TIMESTAMP time_now)
@@ -48,6 +57,8 @@ function(hunter_lock)
       "    Install tag: ${HUNTER_INSTALL_TAG}\n"
       "    Build start at: ${time_now}"
   )
+
+  file(WRITE "${binary_dir_file}" "${PROJECT_BINARY_DIR}")
 
   hunter_verify_toolchain_info()
 endfunction()
