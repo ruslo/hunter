19f59c145e4ca2362696c6e21ff9ceb2fd4d9326
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-02-01 15:40:53 +0400

    Add zlib package

---
+++ cmake/projects/ZLIB/hunter.cmake
@@ -0,0 +1,35 @@
+# Copyright (c) 2014, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED HUNTER_CMAKE_PROJECTS_ZLIB_HUNTER_CMAKE_)
+  return()
+else()
+  set(HUNTER_CMAKE_PROJECTS_ZLIB_HUNTER_CMAKE_ 1)
+endif()
+
+include(hunter_add_version)
+include(hunter_add_version_start)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version_start(GTest)
+
+hunter_add_version(
+    PACKAGE_NAME
+    ZLIB
+    VERSION
+    "1.2.8-hunter"
+    URL
+    "https://github.com/hunter-packages/zlib/archive/v1.2.8-hunter.tar.gz"
+    SHA1
+    75a05fcc928ed52e1eeb93f07a1c78a7890860c0
+)
+
+hunter_pick_scheme(
+    DEFAULT
+    url_sha1_release_debug
+    COMBINED
+    url_sha1_combined_release_debug
+)
+
+hunter_download(PACKAGE_NAME ZLIB)
