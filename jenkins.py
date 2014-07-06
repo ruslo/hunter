c493b6241c9a669f261e629b450e69f895522a06
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-07-06 22:40:33 +0400

    Add testing script

---
+++ jenkins.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2014, Ruslan Baratov
+# All rights reserved.
+
+# https://github.com/ruslo/polly/wiki/Jenkins
+
+def run():
+  print('[hunter testing] Hello')
+
+if __name__ == "__main__":
+  run()
