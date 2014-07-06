2922e4c17cf27c003dfb14bbb788a8def963cb25
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-07-06 22:08:27 +0400

    Update scheme tests

--- cmake/schemes/tests/common.cmake
+++ cmake/schemes/tests/common.cmake
@@ -3,25 +3,26 @@
 
 cmake_minimum_required(VERSION 2.8.12)
 
-set(HUNTER_ROOT "${CMAKE_CURRENT_LIST_DIR}/../../../..")
-include("${HUNTER_ROOT}/Source/cmake/Hunter")
+set(HUNTER_INSTALL_TAG test_default)
+
+set(HUNTER_SELF "${CMAKE_CURRENT_LIST_DIR}/../../..")
+include("${HUNTER_SELF}/cmake/Hunter")
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities.cmake")
 
 # rewrite base for testing
-set(HUNTER_BASE "${PROJECT_BINARY_DIR}/_hunter_base")
+set(HUNTER_BASE "${PROJECT_BINARY_DIR}/_3rdParty/hunter/TestBase")
 
 # temp project location
-set(DOWNLOAD_PROJECT_BASE "${PROJECT_BINARY_DIR}/_download")
+set(DOWNLOAD_PROJECT_BASE "${PROJECT_BINARY_DIR}/_3rdParty/Download")
 
 # describe test download project (URL and SHA1 defined in test CMakeLists.txt)
 set(HUNTER_PACKAGE_NAME dotest)
 set(HUNTER_PACKAGE_BASENAME dotest)
-set(HUNTER_INSTALL_TAG test_default)
 
 # set EP_BASE directory
 set(HUNTER_PACKAGE_INSTALL_DIR "${HUNTER_BASE}/Install")
-set(HUNTER_PACKAGE_DOWNLOAD_DIR "${HUNTER_BASE}/Download/${HUNTER_PACKAGE_NAME}")
+set(HUNTER_PACKAGE_DOWNLOAD_DIR "${CMAKE_CURRENT_LIST_DIR}/_3rdParty/Download")
 set(HUNTER_PACKAGE_SOURCE_DIR "${HUNTER_BASE}/Source/${HUNTER_PACKAGE_NAME}")
 
 # more verbose
