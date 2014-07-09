8723946827da93dea1411fd7b2ca003ea54e1492
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-07-09 19:48:09 +0400

    hunter_lock use hunter_try_lock

--- cmake/modules/hunter_lock.cmake
+++ cmake/modules/hunter_lock.cmake
@@ -1,10 +1,12 @@
 # Copyright (c) 2014, Ruslan Baratov
 # All rights reserved.
 
+cmake_minimum_required(VERSION 3.0)
+
 include(hunter_fatal_error)
 include(hunter_status_debug)
 include(hunter_test_string_not_empty)
-include(hunter_verify_toolchain_info)
+include(hunter_try_lock)
 
 function(hunter_lock)
   # sanity check
@@ -19,49 +21,31 @@ function(hunter_lock)
     return()
   endif()
 
-  hunter_status_debug("lock directory")
-
   set(lock_file "${HUNTER_BASE}/hunter-build.lock")
-  set(binary_dir_file "${HUNTER_BASE}/binary-dir.lock")
 
-  while(EXISTS "${lock_file}")
-    string(TIMESTAMP time_now)
-    if(NOT EXISTS "${binary_dir_file}")
-      hunter_fatal_error("Lock file exists but `${binary_dir_file}` not found")
-    endif()
-    file(READ "${binary_dir_file}" locked_binary_dir)
-    if(EXISTS "${locked_binary_dir}")
-      # directory still exists and may be active
+  while(TRUE)
+    hunter_try_lock(lock_successful)
+    if(lock_successful)
+      return()
+    else()
       file(READ "${lock_file}" build_info)
-      message(
-        "\n"
-        "Another project is using directory right now:\n\n"
-        "    ${HUNTER_BASE}\n\n"
-        "Build info: \n\n"
-        "${build_info}\n\n"
-        "If that build cancelled (interrupted by user or some other reason), "
-        "please remove this file manually:\n\n"
-        "    ${lock_file}\n\n"
-        "Then run CMake again."
+      hunter_status_print(
+          "\n"
+          "Another project is using directory right now:\n\n"
+          "    ${HUNTER_BASE}\n\n"
+          "Build info: \n\n"
+          "${build_info}\n\n"
+          "If that build cancelled (interrupted by user or some other reason), "
+          "please remove this file manually:\n\n"
+          "    ${lock_file}\n\n"
+          "Then run CMake again."
       )
+      ### Lets spin
+      string(TIMESTAMP time_now)
+      execute_process(
+          COMMAND "${CMAKE_COMMAND}" -E echo "[${time_now}] Sleep for 5 sec..."
+      )
+      execute_process(COMMAND "${CMAKE_COMMAND}" -E sleep 5)
     endif()
-    ### Lets spin
-    execute_process(
-        COMMAND "${CMAKE_COMMAND}" -E echo "[${time_now}] Sleep for 5 sec..." 5
-    )
-    execute_process(COMMAND "${CMAKE_COMMAND}" -E sleep 5)
   endwhile()
-
-  string(TIMESTAMP time_now)
-
-  file(
-      WRITE
-      "${lock_file}"
-      "    Project binary directory: ${PROJECT_BINARY_DIR}\n"
-      "    Package name: ${HUNTER_PACKAGE_NAME}\n"
-      "    Install tag: ${HUNTER_INSTALL_TAG}\n"
-      "    Build start at: ${time_now}"
-  )
-
-  file(WRITE "${binary_dir_file}" "${PROJECT_BINARY_DIR}")
 endfunction()
