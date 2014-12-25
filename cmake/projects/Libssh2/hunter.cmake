0692caf5f92179ff3ffb8bf676b93105f63eaf32
Alexander Lamaison <alexander.lamaison@gmail.com>
2014-12-25 10:55:29 +0000

    Added libssh2 SSH/SFTP C library library.

---
+++ cmake/projects/Libssh2/hunter.cmake
@@ -0,0 +1,42 @@
+# Copyright (c) 2014, Alexander Lamaison
+# All rights reserved.
+
+if(DEFINED HUNTER_CMAKE_PROJECTS_LIBSSH2_HUNTER_CMAKE_)
+  return()
+else()
+  set(HUNTER_CMAKE_PROJECTS_LIBSSH2_HUNTER_CMAKE_ 1)
+endif()
+
+include(hunter_add_version)
+include(hunter_add_version_start)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version_start(Libssh2)
+
+hunter_add_version(
+    PACKAGE_NAME
+    Libssh2
+    VERSION
+    "1.4.4_DEV-cmake"
+    URL
+    "https://github.com/hunter-packages/libssh2/archive/v1.4.4-patch-1.tar.gz"
+    SHA1
+    4a5bb23ff19218f92af6e15e46ca5d997c3d7680
+)
+
+hunter_pick_scheme(
+    DEFAULT url_sha1_release_debug
+    COMBINED url_sha1_combined_release_debug
+)
+
+hunter_cmake_args(
+    Libssh2
+    CMAKE_ARGS
+      BUILD_TESTING=OFF
+      BUILD_EXAMPLES=OFF
+      HUNTER_ENABLED=ON
+)
+
+hunter_download(PACKAGE_NAME Libssh2)
