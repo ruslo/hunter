6475dde309c92449368504df73b48ce925f12dfe
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-07 21:46:11 +0200

    OpenSSL: fix version parsing

--- cmake/find/FindOpenSSL.cmake
+++ cmake/find/FindOpenSSL.cmake
@@ -272,7 +272,16 @@ if (OPENSSL_INCLUDE_DIR)
     set(OPENSSL_VERSION "${_OPENSSL_VERSION}")
   elseif(OPENSSL_INCLUDE_DIR AND EXISTS "${OPENSSL_INCLUDE_DIR}/openssl/opensslv.h")
     file(STRINGS "${OPENSSL_INCLUDE_DIR}/openssl/opensslv.h" openssl_version_str
-         REGEX "^#define[\t ]+OPENSSL_VERSION_NUMBER[\t ]+0x([0-9a-fA-F])+.*")
+         REGEX "^#[\t ]*define[\t ]+OPENSSL_VERSION_NUMBER[\t ]+0x([0-9a-fA-F])+.*")
+
+    string(COMPARE EQUAL "${openssl_version_str}" "" _is_empty)
+    if(_is_empty)
+      message(
+          FATAL_ERROR
+          "Incorrect OPENSSL_VERSION_NUMBER define in header"
+          ": ${OPENSSL_INCLUDE_DIR}/openssl/opensslv.h"
+      )
+    endif()
 
     # The version number is encoded as 0xMNNFFPPS: major minor fix patch status
     # The status gives if this is a developer or prerelease and is ignored here.
