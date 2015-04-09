e9a09ffb93eb515db434228af0f9e4be57bc41a3
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-09 23:16:22 +0200

    hunter_wiki: short format

--- cmake/modules/hunter_wiki.cmake
+++ cmake/modules/hunter_wiki.cmake
@@ -1,9 +1,9 @@
-# Copyright (c) 2014, Ruslan Baratov
+# Copyright (c) 2014-2015, Ruslan Baratov
 # All rights reserved.
 
 function(hunter_wiki wiki_page)
   message("------------------------------ WIKI -------------------------------")
-  message("    ${wiki_page}")
+  message("    ${HUNTER_WIKI}/${wiki_page}")
   message("-------------------------------------------------------------------")
   message("")
   message(FATAL_ERROR "")
