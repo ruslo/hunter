d9307d6a488cf68400c9a7242fe5ecc64b24c2f2
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-10 14:00:00 +0200

    Print long lines

--- cmake/modules/hunter_status_debug.cmake
+++ cmake/modules/hunter_status_debug.cmake
@@ -2,10 +2,8 @@
 # All rights reserved.
 
 function(hunter_status_debug)
-  foreach(print_message ${ARGV})
-    if(HUNTER_STATUS_DEBUG)
-      string(TIMESTAMP timestamp)
-      message(STATUS "[hunter *** DEBUG *** ${timestamp}] ${print_message}")
-    endif()
-  endforeach()
+  if(HUNTER_STATUS_DEBUG)
+    string(TIMESTAMP timestamp)
+    message(STATUS "[hunter *** DEBUG *** ${timestamp}] ${ARGV}")
+  endif()
 endfunction()
--- cmake/modules/hunter_status_print.cmake
+++ cmake/modules/hunter_status_print.cmake
@@ -1,10 +1,8 @@
-# Copyright (c) 2013, Ruslan Baratov
+# Copyright (c) 2013, 2015 Ruslan Baratov
 # All rights reserved.
 
 function(hunter_status_print)
-  foreach(print_message ${ARGV})
-    if(HUNTER_STATUS_PRINT OR HUNTER_STATUS_DEBUG)
-      message(STATUS "[hunter] ${print_message}")
-    endif()
-  endforeach()
+  if(HUNTER_STATUS_PRINT OR HUNTER_STATUS_DEBUG)
+    message(STATUS "[hunter] ${ARGV}")
+  endif()
 endfunction()
