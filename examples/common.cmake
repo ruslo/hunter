30ea8cb1d3d419f4275bf754fa998d7752acc089
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-10 19:42:55 +0200

    examples/common.cmake: apply new gate variables

--- examples/common.cmake
+++ examples/common.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2013, Ruslan Baratov
+# Copyright (c) 2013, 2015 Ruslan Baratov
 # All rights reserved.
 
 cmake_minimum_required(VERSION 3.0)
@@ -6,6 +6,13 @@ cmake_minimum_required(VERSION 3.0)
 if(NOT HUNTER_ROOT)
   message(FATAL_ERROR "Expected HUNTER_ROOT")
 endif()
+
+# Emulate HunterGate
+set(HUNTER_GATE_ROOT "${HUNTER_ROOT}")
+set(_unused "xxxxxxx")
+set(HUNTER_GATE_VERSION "${_unused}")
+set(HUNTER_GATE_SHA1 "${_unused}")
+# -- end
 
 include("${HUNTER_ROOT}/cmake/Hunter")
 include(hunter_add_package)
