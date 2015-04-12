5e01bcbfb18cb95f02e42d6b5ce350a5663465f4
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-12 12:09:21 +0200

    hunter_gate_settings: check HUNTER_GATE_DONE is YES

--- cmake/modules/hunter_gate_settings.cmake
+++ cmake/modules/hunter_gate_settings.cmake
@@ -1,6 +1,8 @@
 # Copyright (c) 2015, Ruslan Baratov
 # All rights reserved.
 
+include(hunter_internal_error)
+
 function(hunter_gate_settings varname)
   set(settings "")
   foreach(
@@ -10,9 +12,18 @@ function(hunter_gate_settings varname)
       HUNTER_CONFIG_SHA1
       HUNTER_VERSION
       HUNTER_TOOLCHAIN_SHA1
+      HUNTER_STATUS_DEBUG
   )
     list(APPEND settings "-D${hunter_var}=${${hunter_var}}")
   endforeach()
+
+  if(NOT HUNTER_GATE_DONE)
+    get_property(_hunter_gate_done GLOBAL PROPERTY HUNTER_GATE_DONE SET)
+    if(NOT _hunter_gate_done)
+      hunter_internal_error("HUNTER_GATE_DONE is OFF")
+    endif()
+  endif()
+
   list(APPEND settings "-DHUNTER_GATE_DONE=YES")
   set("${varname}" "${settings}" PARENT_SCOPE)
 endfunction()
