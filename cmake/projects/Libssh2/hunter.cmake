7982780e15c0a685ad54e6726fd2e4409c12e991
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-12 16:22:27 +0200

    Update Libssh2

--- cmake/projects/Libssh2/hunter.cmake
+++ cmake/projects/Libssh2/hunter.cmake
@@ -16,6 +16,17 @@ hunter_add_version(
     PACKAGE_NAME
     Libssh2
     VERSION
+    "1.4.4-patch-2"
+    URL
+    "https://github.com/hunter-packages/libssh2/archive/v1.4.4-patch-2.tar.gz"
+    SHA1
+    3334d745eb6ed5e3fb860a5a74e9cfc2e4576c81
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    Libssh2
+    VERSION
     "1.4.4_DEV-cmake"
     URL
     "https://github.com/hunter-packages/libssh2/archive/v1.4.4-patch-1.tar.gz"
