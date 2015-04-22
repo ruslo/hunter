afbf3f50facdf6be4fbd3cdc35135f495cc8e4f8
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-22 21:40:50 +0200

    hunter_check_flush_needed: beautify log messages (more)

--- cmake/modules/hunter_check_flush_needed.cmake
+++ cmake/modules/hunter_check_flush_needed.cmake
@@ -13,21 +13,17 @@ function(hunter_check_flush_needed hunter_self flush_done)
 
   string(COMPARE EQUAL "${HUNTER_CACHED_ROOT}" "${HUNTER_GATE_ROOT}" is_ok)
   if(NOT is_ok)
-    hunter_status_debug(
-        "HUNTER_ROOT changed:\n"
-        "  ${HUNTER_CACHED_ROOT}\n"
-        "  ${HUNTER_GATE_ROOT}"
-    )
+    hunter_status_debug("HUNTER_ROOT changed:")
+    hunter_status_debug("  ${HUNTER_CACHED_ROOT}")
+    hunter_status_debug("  ${HUNTER_GATE_ROOT}")
     set(flush TRUE)
   endif()
 
   string(COMPARE EQUAL "${HUNTER_SHA1}" "${HUNTER_GATE_SHA1}" is_ok)
   if(NOT is_ok)
-    hunter_status_debug(
-        "HUNTER_SHA1 changed:\n"
-        "  ${HUNTER_SHA1}\n"
-        "  ${HUNTER_GATE_SHA1}"
-    )
+    hunter_status_debug("HUNTER_SHA1 changed:")
+    hunter_status_debug("  ${HUNTER_SHA1}")
+    hunter_status_debug("  ${HUNTER_GATE_SHA1}")
     set(flush TRUE)
   endif()
 
@@ -35,11 +31,9 @@ function(hunter_check_flush_needed hunter_self flush_done)
       COMPARE EQUAL "${HUNTER_CONFIG_SHA1}" "${HUNTER_GATE_CONFIG_SHA1}" is_ok
   )
   if(NOT is_ok)
-    hunter_status_debug(
-        "HUNTER_CONFIG_SHA1 changed:\n"
-        "  ${HUNTER_CONFIG_SHA1}\n"
-        "  ${HUNTER_GATE_CONFIG_SHA1}"
-    )
+    hunter_status_debug("HUNTER_CONFIG_SHA1 changed:")
+    hunter_status_debug("  ${HUNTER_CONFIG_SHA1}")
+    hunter_status_debug("  ${HUNTER_GATE_CONFIG_SHA1}")
     set(flush TRUE)
   endif()
 
@@ -47,11 +41,9 @@ function(hunter_check_flush_needed hunter_self flush_done)
       COMPARE EQUAL "${HUNTER_VERSION}" "${HUNTER_GATE_VERSION}" is_ok
   )
   if(NOT is_ok)
-    hunter_status_debug(
-        "HUNTER_VERSION changed:\n"
-        "  ${HUNTER_VERSION}\n"
-        "  ${HUNTER_GATE_VERSION}"
-    )
+    hunter_status_debug("HUNTER_VERSION changed:")
+    hunter_status_debug("  ${HUNTER_VERSION}")
+    hunter_status_debug("  ${HUNTER_GATE_VERSION}")
     set(flush TRUE)
   endif()
 
@@ -63,11 +55,9 @@ function(hunter_check_flush_needed hunter_self flush_done)
       is_ok
   )
   if(NOT is_ok)
-    hunter_status_debug(
-        "HUNTER_TOOLCHAIN_SHA1 changed:\n"
-        "  ${HUNTER_TOOLCHAIN_SHA1}\n"
-        "  ${HUNTER_GATE_TOOLCHAIN_SHA1}"
-    )
+    hunter_status_debug("HUNTER_TOOLCHAIN_SHA1 changed:")
+    hunter_status_debug("  ${HUNTER_TOOLCHAIN_SHA1}")
+    hunter_status_debug("  ${HUNTER_GATE_TOOLCHAIN_SHA1}")
     set(flush TRUE)
   endif()
 
@@ -79,11 +69,9 @@ function(hunter_check_flush_needed hunter_self flush_done)
       is_ok
   )
   if(NOT is_ok)
-    hunter_status_debug(
-        "HUNTER_CONFIGURATION_TYPES changed:\n"
-        "  ${HUNTER_CONFIGURATION_TYPES}\n"
-        "  ${HUNTER_CACHED_CONFIGURATION_TYPES}"
-    )
+    hunter_status_debug("HUNTER_CONFIGURATION_TYPES changed:")
+    hunter_status_debug("  ${HUNTER_CONFIGURATION_TYPES}")
+    hunter_status_debug("  ${HUNTER_CACHED_CONFIGURATION_TYPES}")
     set(flush TRUE)
   endif()
 
