34bb532a184ddc2be5603f1cedaad888be9a9357
David Hirvonen <dhirvonen@elucideye.com>
2015-04-25 16:01:28 -0400

    add v1.0.19 for cvmatio

--- cmake/projects/cvmatio/hunter.cmake
+++ cmake/projects/cvmatio/hunter.cmake
@@ -14,31 +14,32 @@ include(hunter_download)
 include(hunter_pick_scheme)
 include(hunter_add_package)
 
-hunter_add_package(TIFF)
-hunter_add_package(OpenCV)
-hunter_add_package(ZLIB)
-hunter_add_package(Boost COMPONENTS filesystem system serialization)
-
-# List of versions here...
+#hunter_add_package(TIFF)
+#hunter_add_package(OpenCV)
+#hunter_add_package(ZLIB)
+#hunter_add_package(Boost COMPONENTS filesystem system serialization)
 
 hunter_add_version(
     PACKAGE_NAME
     cvmatio
     VERSION
     "1.0.18"
-    URL
+     URL
     "https://github.com/headupinclouds/cvmatio/archive/v1.0.18.tar.gz"
-    SHA1
+     SHA1
     4bf8f9e9edc34a346220dc8ea307185d95283e93
-)
+ )
 
-# Pick a download scheme
-# hunter_pick_scheme(
-#     DEFAULT
-#     url_sha1_release_debug # For separate release/debug generators, like Makefile
-#     COMBINED
-#     url_sha1_combined_release_debug # For Xcode and Visual Studio
-# )
+hunter_add_version(
+    PACKAGE_NAME
+    cvmatio
+    VERSION
+    "1.0.19"
+    URL
+    "https://github.com/headupinclouds/cvmatio/archive/v1.0.19.tar.gz"
+    SHA1
+    1c0760044d623ff1b4ee98873aaea222fb328234
+)
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 
