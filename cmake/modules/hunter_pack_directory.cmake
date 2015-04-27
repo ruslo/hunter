aade8c7837f6554969bfe2c979275655f7e6b876
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-27 23:54:46 +0200

    Add new module: hunter_pack_directory

---
+++ cmake/modules/hunter_pack_directory.cmake
@@ -0,0 +1,55 @@
+# Copyright (c) 2015, Ruslan Baratov
+# All rights reserved.
+
+include(hunter_internal_error)
+include(hunter_print_cmd)
+include(hunter_status_debug)
+include(hunter_status_print)
+
+function(hunter_pack_directory dir_to_pack dest_dir)
+  file(MAKE_DIRECTORY "${dest_dir}")
+  set(temp "${dest_dir}/cache.tar.bz2")
+
+  set(cmd "${CMAKE_COMMAND}" "-E" "tar")
+  if(HUNTER_STATUS_DEBUG)
+    list(APPEND cmd "cvjf")
+  else()
+    list(APPEND cmd "cjf")
+  endif()
+
+  list(APPEND cmd "${temp}" "--")
+
+  file(GLOB dir_list RELATIVE "${dir_to_pack}" "${dir_to_pack}/*")
+  foreach(x ${dir_list})
+    list(APPEND cmd "${x}")
+  endforeach()
+
+  if(HUNTER_STATUS_DEBUG)
+    set(logging_params "")
+  elseif(HUNTER_STATUS_PRINT)
+    set(logging_params "")
+  else()
+    set(logging_params "OUTPUT_QUIET")
+  endif()
+
+  hunter_print_cmd("${dir_to_pack}" "${cmd}")
+
+  execute_process(
+      COMMAND ${cmd}
+      WORKING_DIRECTORY "${dir_to_pack}"
+      RESULT_VARIABLE packing_result
+      ${logging_params}
+  )
+
+  if(packing_result EQUAL 0)
+    hunter_status_debug("Packing successful: ${temp})")
+  else()
+    hunter_internal_error("Packing failed")
+  endif()
+
+  file(SHA1 "${temp}" archive_sha1)
+  set(dest_archive "${dest_dir}/${archive_sha1}.tar.bz2")
+  file(RENAME "${temp}" "${dest_archive}")
+
+  hunter_status_print("Cache saved: ${dest_archive}")
+endfunction()
