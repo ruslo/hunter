8f1b0f9bb18edd390df989105684007fa8e6d525
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-05-01 15:23:37 +0200

    New module: hunter_cache_run

---
+++ cmake/modules/hunter_cache_run.cmake
@@ -0,0 +1,42 @@
+# Copyright (c) 2015, Ruslan Baratov
+# All rights reserved.
+
+include(CMakeParseArguments) # cmake_parse_arguments
+
+include(hunter_add_package)
+include(hunter_internal_error)
+include(hunter_test_string_not_empty)
+
+# Do not install any packages.
+# Check cache and load cache.sha1 to home directory on hit.
+function(hunter_cache_run)
+  set(one PACKAGE COMPONENT)
+  cmake_parse_arguments(x "" "${one}" "" "${ARGV}")
+  # -> x_PACKAGE
+  # -> x_COMPONENT
+
+  hunter_test_string_not_empty("${x_PACKAGE}")
+
+  string(COMPARE NOTEQUAL "${x_COMPONENT}" "" has_component)
+  string(COMPARE NOTEQUAL "${x_UNPARSED_ARGUMENTS}" "" has_unparsed)
+  if(has_unparsed)
+    hunter_internal_error("Unparsed: ${x_UNPARSED_ARGUMENTS}")
+  endif()
+
+  list(REMOVE_ITEM HUNTER_PARENT_PACKAGE "")
+  string(COMPARE EQUAL "${HUNTER_PARENT_PACKAGE}" "" is_empty)
+  if(NOT is_empty)
+    hunter_internal_error("HUNTER_PARENT_PACKAGE is not empty")
+  endif()
+
+  if(HUNTER_CACHE_RUN)
+    return()
+  endif()
+
+  set(HUNTER_CACHE_RUN TRUE)
+  if(has_component)
+    hunter_add_package(${x_PACKAGE})
+  else()
+    hunter_add_package(${x_PACKAGE} COMPONENTS ${x_COMPONENT})
+  endif()
+endfunction()
