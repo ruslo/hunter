30881b439ae2c8c11af0d9e7aad74cbb96743ab9
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-05-01 19:16:29 +0200

    Stand-alone examples run

--- examples/common.cmake
+++ examples/common.cmake
@@ -19,25 +19,16 @@ message("Including HunterGate: ${gate_module}")
 include("${gate_module}")
 
 ### Check testing variables are set
-string(COMPARE EQUAL "${TESTING_URL}" "" is_empty)
-if(is_empty)
-  message(FATAL_ERROR "TESTING_URL variable is empty (see jenkins.py script)")
+string(COMPARE EQUAL "${TESTING_URL}" "" url_is_empty)
+string(COMPARE EQUAL "${TESTING_SHA1}" "" sha1_is_empty)
+string(COMPARE EQUAL "${HUNTER_ROOT}" "" hunter_root_is_empty)
+
+if(NOT url_is_empty AND NOT sha1_is_empty AND NOT hunter_root_is_empty)
+  get_filename_component(TESTING_URL "${TESTING_URL}" ABSOLUTE)
+
+  ### HunterGate module
+  HunterGate(URL "${TESTING_URL}" SHA1 "${TESTING_SHA1}")
+else()
+  get_filename_component(HUNTER_ROOT "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
+  HunterGate(URL "x" SHA1 "x")
 endif()
-
-string(COMPARE EQUAL "${TESTING_SHA1}" "" is_empty)
-if(is_empty)
-  message(FATAL_ERROR "TESTING_SHA1 variable is empty (see jenkins.py script)")
-endif()
-
-string(COMPARE EQUAL "${HUNTER_ROOT}" "" is_empty)
-if(is_empty)
-  message(FATAL_ERROR "HUNTER_ROOT variable is empty (see jenkins.py script)")
-endif()
-
-get_filename_component(TESTING_URL "${TESTING_URL}" ABSOLUTE)
-
-### HunterGate module
-HunterGate(
-    URL "${TESTING_URL}"
-    SHA1 "${TESTING_SHA1}"
-)
