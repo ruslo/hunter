0735baa3e825caa6c1518a108b66b386a5a0c197
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-05-17 20:37:23 +0200

    Cacheable GPUImage

--- cmake/projects/GPUImage/hunter.cmake
+++ cmake/projects/GPUImage/hunter.cmake
@@ -8,6 +8,7 @@ else()
 endif()
 
 include(hunter_add_version)
+include(hunter_cacheable)
 include(hunter_cmake_args)
 include(hunter_download)
 include(hunter_pick_scheme)
@@ -29,4 +30,5 @@ hunter_cmake_args(
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(GPUImage)
 hunter_download(PACKAGE_NAME GPUImage)
