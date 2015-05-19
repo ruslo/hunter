332cb7a1c8302fec1dbd834f58933d016f29e7c1
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-05-19 20:00:17 +0200

    New package: Android-Support-Repository

---
+++ cmake/projects/Android-Support-Repository/hunter.cmake
@@ -0,0 +1,26 @@
+# Copyright (c) 2015, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED HUNTER_CMAKE_PROJECTS_ANDROID_SUPPORT_REPOSITORY_HUNTER_CMAKE_)
+  return()
+else()
+  set(HUNTER_CMAKE_PROJECTS_ANDROID_SUPPORT_REPOSITORY_HUNTER_CMAKE_ 1)
+endif()
+
+include(hunter_add_version)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    Android-Support-Repository
+    VERSION
+    "14"
+    URL
+    "http://dl-ssl.google.com/android/repository/android_m2repository_r14.zip"
+    SHA1
+    0011dfe1473ccdfb1a533a19cad152c59dcd6b45
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_unpack)
+hunter_download(PACKAGE_NAME Android-Support-Repository)
