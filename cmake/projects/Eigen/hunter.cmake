166ad21bd42334b4f2fa1ca0f2aacb7ac5a5f8cf
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-06-02 14:01:04 +0200

    Update Eigen: 3.2.4-p0

--- cmake/projects/Eigen/hunter.cmake
+++ cmake/projects/Eigen/hunter.cmake
@@ -9,10 +9,22 @@ endif()
 
 # Load used modules
 include(hunter_add_version)
+include(hunter_cmake_args)
 include(hunter_download)
 include(hunter_pick_scheme)
 
 # List of versions here...
+hunter_add_version(
+    PACKAGE_NAME
+    Eigen
+    VERSION
+    "3.2.4-p0"
+    URL
+    "https://github.com/hunter-packages/eigen/archive/v3.2.4-p0.tar.gz"
+    SHA1
+    6fd638939dfd3a3be1cf0ea920160591ed60f76d
+)
+
 hunter_add_version(
     PACKAGE_NAME
     Eigen
@@ -23,6 +35,8 @@ hunter_add_version(
     SHA1
     b826f665bdfe31784795eef79c75540db71ab702
 )
+
+hunter_cmake_args(Eigen CMAKE_ARGS EIGEN_ENABLE_TESTING=OFF)
 
 # Pick a download scheme
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
