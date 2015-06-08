9837c963ebf835709d6e5c9ecf63cd2bc25a042c
Alexandre Pretyman <pretyman@users.noreply.github.com>
2015-06-07 22:58:36 -0400

    Moved autotools from scheme to modules directory

---
+++ cmake/modules/autotools_append_compiler_cxxflags.cmake
@@ -0,0 +1,17 @@
+# Copyright (c) 2015 Ruslan Baratov, Alexandre Pretyman
+# All rights reserved.
+#
+# Adds to append_result 
+#   CXXFLAGS=${cxxflags} ${CMAKE_CXX_FLAGS}
+#
+function(autotools_append_compiler_cxxflags cxxflags append_result)
+  #C++ Compiler flags (defines or include directories should not be needed here) 
+  set(_cxxflags "${cxxflags} ${CMAKE_CXX_FLAGS}")
+  string(STRIP "${_cxxflags}" _cxxflags)
+  string(COMPARE NOTEQUAL "${_cxxflags}" "" has_cxxflags)
+  if (has_cxxflags)
+    set(${append_result} ${${append_result}} CXXFLAGS=${_cxxflags} PARENT_SCOPE)
+  endif()
+endfunction()
+
+
---
+++ cmake/modules/autotools_append_linker_flags.cmake
@@ -0,0 +1,14 @@
+# Copyright (c) 2015 Ruslan Baratov, Alexandre Pretyman
+# All rights reserved.
+#
+# Adds to append_result 
+#   LDFLAGS=${ldflags} 
+#
+function(autotools_append_linker_flags ldflags append_result)
+  #Linker flags
+  string(STRIP "${ldflags}" ldflags)
+  string(COMPARE NOTEQUAL "${ldflags}" "" has_ldflags)
+  if(has_ldflags)
+    set(${append_result} ${${append_result}} LDFLAGS=${ldflags} PARENT_SCOPE)
+  endif()
+endfunction()
