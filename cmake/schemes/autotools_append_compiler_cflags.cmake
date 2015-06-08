cf6cf131b14e141f5171868312e83bcc0a20b5f5
Alexandre Pretyman <pretyman@users.noreply.github.com>
2015-06-07 22:54:42 -0400

    Autotools: fixed pass of CFLAGS + tabs to 2 spaces

--- cmake/schemes/autotools_append_compiler_cflags.cmake
+++ cmake/schemes/autotools_append_compiler_cflags.cmake
@@ -2,14 +2,14 @@
 # All rights reserved.
 #
 # Adds to append_result 
-# 	CFLAGS=${cflags} ${CMAKE_C_FLAGS}
+#   CFLAGS=${cflags} ${CMAKE_C_FLAGS}
 #
 function(autotools_append_compiler_cflags cflags append_result)
-	#C Compiler Flags (defines or include directories should not be needed here)
-	set(_cflags "${cflags} ${CMAKE_C_FLAGS}")
-	string(STRIP "${_cflags}" _cflags)
-	string(COMPARE NOTEQUAL "${_cflags}" "" has_cflags)
-	if (has_cflags)
-	  set(${append_result} ${${append_result}} CFLAGS=${cflags})
-	endif()
+  #C Compiler Flags (defines or include directories should not be needed here)
+  set(_cflags "${cflags} ${CMAKE_C_FLAGS}")
+  string(STRIP "${_cflags}" _cflags)
+  string(COMPARE NOTEQUAL "${_cflags}" "" has_cflags)
+  if (has_cflags)
+		set(${append_result} ${${append_result}} CFLAGS=${_cflags} PARENT_SCOPE)
+  endif()
 endfunction()
--- cmake/schemes/autotools_append_linker_flags.cmake
+++ cmake/schemes/autotools_append_linker_flags.cmake
@@ -2,13 +2,13 @@
 # All rights reserved.
 #
 # Adds to append_result 
-# 	LDFLAGS=${ldflags} 
+#   LDFLAGS=${ldflags} 
 
 function(autotools_append_linker_flags ldflags append_result)
-	#Linker flags
-	string(STRIP "${ldflags}" ldflags)
-	string(COMPARE NOTEQUAL "${ldflags}" "" has_ldflags)
-	if(has_ldflags)
-		set(${append_result} ${${append_result}} LDFLAGS=${ldflags} PARENT_SCOPE)
-	endif()
+  #Linker flags
+  string(STRIP "${ldflags}" ldflags)
+  string(COMPARE NOTEQUAL "${ldflags}" "" has_ldflags)
+  if(has_ldflags)
+    set(${append_result} ${${append_result}} LDFLAGS=${ldflags} PARENT_SCOPE)
+  endif()
 endfunction()
