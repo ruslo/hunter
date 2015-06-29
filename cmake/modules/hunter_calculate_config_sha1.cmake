0cc25d704732edefe4c9cd6d7cd5bf8e72d2fd5b
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-06-29 15:46:59 +0300

    Change scope of config-id calculation
    Do not use script to calculate config-id since we need some variables from
    toolchain be visible (like ANDROID). Calculate config-id in the user's scope.

--- cmake/modules/hunter_calculate_config_sha1.cmake
+++ cmake/modules/hunter_calculate_config_sha1.cmake
@@ -16,53 +16,95 @@ function(hunter_calculate_config_sha1 hunter_self hunter_base user_config)
   hunter_test_string_not_empty("${hunter_self}")
   hunter_test_string_not_empty("${hunter_base}")
   hunter_test_string_not_empty("${user_config}")
-
-  if(NOT EXISTS "${user_config}")
-    hunter_internal_error("Hunter config not exists")
-  endif()
+  hunter_test_string_not_empty("${CMAKE_BINARY_DIR}")
 
   hunter_status_print("Calculating Config-SHA1")
 
-  set(script "${hunter_self}/scripts/calc-config-sha1.cmake")
-  if(NOT EXISTS "${script}")
-    hunter_internal_error("${script} not exists")
+  # Include default_config
+  set(default_config "${hunter_self}/cmake/configs/default.cmake")
+  if(NOT EXISTS "${default_config}")
+    hunter_internal_error("File `${default_config}` not exists")
+  endif()
+  set(HUNTER_ALLOW_CONFIG_LOADING YES)
+  include("${default_config}")
+  set(HUNTER_ALLOW_CONFIG_LOADING NO)
+
+  # Include user_config
+  if(NOT EXISTS "${user_config}")
+    hunter_internal_error("Hunter config not exists")
+  endif()
+  set(HUNTER_ALLOW_CONFIG_LOADING YES)
+  include("${user_config}")
+  set(HUNTER_ALLOW_CONFIG_LOADING NO)
+
+  # Create list of the projects
+  set(directory_with_projects "${hunter_self}/cmake/projects")
+  if(NOT EXISTS "${directory_with_projects}")
+    hunter_internal_error("Directory `${directory_with_projects}` not exists")
+  endif()
+  if(NOT IS_DIRECTORY "${directory_with_projects}")
+    hunter_internal_error(
+        "Path `${directory_with_projects}` is not a directory"
+    )
   endif()
 
+  file(
+      GLOB projects
+      RELATIVE "${directory_with_projects}"
+      "${directory_with_projects}/*"
+  )
+
+  if(NOT projects)
+    hunter_internal_error("No projects found")
+  endif()
+
+  set(real_projects "")
+  foreach(x ${projects})
+    if(IS_DIRECTORY "${directory_with_projects}/${x}")
+      list(APPEND real_projects "${x}")
+    endif()
+  endforeach()
+
+  if(NOT real_projects)
+    hunter_internal_error("No projects found")
+  endif()
+
+  set(projects "${real_projects}")
+  list(SORT projects)
+
+  # Create unified version
   set(work_dir "${CMAKE_BINARY_DIR}/_3rdParty/Hunter/config-id")
   file(REMOVE_RECURSE "${work_dir}")
   file(MAKE_DIRECTORY "${work_dir}")
 
-  set(default_config "${hunter_self}/cmake/configs/default.cmake")
-  set(directory_with_projects "${hunter_self}/cmake/projects")
-
-  set(
-      cmd
-      "${CMAKE_COMMAND}"
-      "-DDEFAULT_CONFIG=${default_config}"
-      "-DUSER_CONFIG=${user_config}"
-      "-DDIRECTORY_WITH_PROJECTS=${directory_with_projects}"
-      "-DTEMP_DIRECTORY=${work_dir}"
-      "-DHUNTER_SELF=${hunter_self}"
-      -P
-      "${script}"
-  )
-
-  hunter_print_cmd("${work_dir}" "${cmd}")
-
-  execute_process(
-      COMMAND ${cmd}
-      WORKING_DIRECTORY "${work_dir}"
-      RESULT_VARIABLE result
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-
-  if(NOT result EQUAL 0)
-    hunter_user_error("User config `${user_config}` is not correct.")
-  endif()
-
-  if(NOT EXISTS "${work_dir}/config.cmake")
-    hunter_internal_error("config.cmake not generated")
-  endif()
+  set(input_file "${work_dir}/config.cmake")
+  file(WRITE "${input_file}" "include(hunter_config)\n")
+  foreach(x ${projects})
+    set(version "${HUNTER_${x}_VERSION}")
+    if(version)
+      file(
+          APPEND
+          "${input_file}"
+          "hunter_config(${x} "
+          "VERSION ${version}"
+      )
+      string(COMPARE NOTEQUAL "${HUNTER_${x}_CMAKE_ARGS}" "" have_args)
+      if(have_args)
+        file(APPEND "${input_file}" " CMAKE_ARGS")
+        foreach(y ${HUNTER_${x}_CMAKE_ARGS})
+          file(APPEND "${input_file}" " ${y}")
+        endforeach()
+      endif()
+      string(COMPARE NOTEQUAL "${HUNTER_${x}_CONFIGURATION_TYPES}" "" have_types)
+      if(have_types)
+        file(APPEND "${input_file}" " CONFIGURATION_TYPES")
+        foreach(y ${HUNTER_${x}_CONFIGURATION_TYPES})
+          file(APPEND "${input_file}" " ${y}")
+        endforeach()
+      endif()
+      file(APPEND "${input_file}" ")\n")
+    endif()
+  endforeach()
 
   file(SHA1 "${work_dir}/config.cmake" HUNTER_GATE_CONFIG_SHA1)
   set(HUNTER_GATE_CONFIG_SHA1 "${HUNTER_GATE_CONFIG_SHA1}" PARENT_SCOPE)
