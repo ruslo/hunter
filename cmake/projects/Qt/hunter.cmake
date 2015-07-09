3bed8797aa3277aaf56396eeb04c5dd1336e3764
Alexandre Pretyman <pretyman@users.noreply.github.com>
2015-07-09 00:28:25 -0400

    Added Qt project hunter file

---
+++ cmake/projects/Qt/hunter.cmake
@@ -0,0 +1,26 @@
+# Copyright (c) 2015, Ruslan Baratov, Alexandre Pretyman
+# All rights reserved.
+
+if(DEFINED HUNTER_CMAKE_PROJECTS_QT_HUNTER_CMAKE)
+  return()
+else()
+  set(HUNTER_CMAKE_PROJECTS_QT_HUNTER_CMAKE 1)
+endif()
+
+include(hunter_add_version)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    Qt
+    VERSION
+    "5.5.0"
+    URL
+    "http://download.qt.io/official_releases/qt/5.5/5.5.0/single/qt-everywhere-opensource-src-5.5.0.tar.gz"
+    SHA1
+    5a11602e781ef3160965dce9e959b2d4f93e6def
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_qt)
+hunter_download(PACKAGE_NAME Qt)
