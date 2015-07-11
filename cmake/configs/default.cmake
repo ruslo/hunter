f48778a8ce13c8a11654197efcb83a051e45cd2a
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-07-11 19:07:05 +0300

    Update PNG

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -61,7 +61,7 @@ hunter_config(Libssh2 VERSION 1.4.4-patch-2)
 hunter_config(OpenCV VERSION 3.0.0-p4)
 hunter_config(OpenCV-Extra VERSION 3.0.0)
 hunter_config(OpenSSL VERSION 1.0.2a)
-hunter_config(PNG VERSION 1.6.16-p3)
+hunter_config(PNG VERSION 1.6.16-p4)
 hunter_config(RapidJSON VERSION 0.11-hunter)
 hunter_config(SQLite3 VERSION autoconf-3080803) #R-Tree enabled
 hunter_config(Sober VERSION 0.1.2)
