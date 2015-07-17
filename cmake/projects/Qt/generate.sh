31ae6739f77fe5909db25d5aa1dc6b77ad047808
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-07-17 14:38:03 +0300

    Qt components: generation script and template

---
+++ cmake/projects/Qt/generate.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+
+# Copyright (c) 2015, Ruslan Baratov
+# All rights reserved.
+
+set -x
+
+QT_LIBS="
+    qt3d
+    qtactiveqt
+    qtandroidextras
+    qtbase
+    qtcanvas3d
+    qtconnectivity
+    qtdeclarative
+    qtdoc
+    qtenginio
+    qtgraphicaleffects
+    qtimageformats
+    qtlocation
+    qtmacextras
+    qtmultimedia
+    qtquick1
+    qtquickcontrols
+    qtscript
+    qtsensors
+    qtserialport
+    qtsvg
+    qttools
+    qttranslations
+    qtwayland
+    qtwebchannel
+    qtwebengine
+    qtwebkit
+    qtwebkit-examples
+    qtwebsockets
+    qtwinextras
+    qtx11extras
+    qtxmlpatterns
+"
+
+for library in ${QT_LIBS};
+do
+  LIBRARY_UPPER=`echo ${library} | tr '[:lower:]' '[:upper:]'`
+  echo "${library} -- ${LIBRARY_UPPER}"
+
+  mkdir -p ${library}
+  cp hunter.cmake.in ${library}/hunter.cmake
+  sed -i.back "s,qt_component,${library}," ${library}/hunter.cmake
+  sed -i.back "s,QT_COMPONENT,${LIBRARY_UPPER},g" ${library}/hunter.cmake
+done
