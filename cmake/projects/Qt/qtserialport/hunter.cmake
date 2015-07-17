ea9e77a7074b78cef92a4c7d08f39aa3f1aff3cb
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-07-17 14:39:24 +0300

    Add automatically generated Qt/<component>/hunter.cmake modules

---
+++ cmake/projects/Qt/qtserialport/hunter.cmake
@@ -0,0 +1,14 @@
+# Copyright (c) 2015, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED HUNTER_CMAKE_PROJECTS_QT_QTSERIALPORT_HUNTER_CMAKE_)
+  return()
+else()
+  set(HUNTER_CMAKE_PROJECTS_QT_QTSERIALPORT_HUNTER_CMAKE_ 1)
+endif()
+
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_pick_scheme(DEFAULT url_sha1_qt)
+hunter_download(PACKAGE_NAME Qt PACKAGE_COMPONENT qtserialport)
