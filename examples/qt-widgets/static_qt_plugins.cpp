58695b92399ec4bf2888cd0c72126d5f961fdcd1
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-07-18 19:39:49 +0300

    Qt example: fix iOS build errors
    [skip ci]

--- examples/qt-widgets/static_qt_plugins.cpp
+++ examples/qt-widgets/static_qt_plugins.cpp
@@ -2,6 +2,8 @@
 
 // http://doc.qt.io/qt-5/qtglobal.html
 
-#if defined(Q_OS_DARWIN)
+#if defined(Q_OS_IOS)
+  // Nothing (exclude OS X)
+#elif defined(Q_OS_DARWIN)
   Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin);
 #endif
