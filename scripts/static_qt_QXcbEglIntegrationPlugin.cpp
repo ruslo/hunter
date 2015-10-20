a4fac2f101c388c3a3965a478c7c08c7efcd435b
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-10-21 01:24:19 +0300

    Add source static_qt_QXcbEglIntegrationPlugin.cpp

---
+++ scripts/static_qt_QXcbEglIntegrationPlugin.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2015, Ruslan Baratov
+// All rights reserved.
+
+#include <QtPlugin>
+
+// http://doc.qt.io/qt-5/qtglobal.html
+
+#if defined(Q_OS_IOS)
+  // TODO
+#elif defined(Q_OS_DARWIN)
+  // TODO
+#elif defined(Q_OS_LINUX)
+  Q_IMPORT_PLUGIN(QXcbEglIntegrationPlugin);
+#elif defined(Q_OS_WIN)
+  // TODO
+#endif
