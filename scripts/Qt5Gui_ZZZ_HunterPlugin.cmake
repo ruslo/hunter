0ea69020addcc7774bfd6b60340b2726c0d40d79
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-10-27 20:03:13 +0600

    Qt: add static_qt_QMngPlugin.cpp

--- scripts/Qt5Gui_ZZZ_HunterPlugin.cmake
+++ scripts/Qt5Gui_ZZZ_HunterPlugin.cmake
@@ -224,3 +224,26 @@ if(TARGET Qt5::QJp2Plugin)
     endif()
   endif()
 endif()
+
+if(TARGET Qt5::QMngPlugin)
+  # QMngPlugin is not always installed (?),
+  # for simplicity check existence of target
+
+  if(_qt5_is_static)
+    if(CMAKE_VERSION VERSION_LESS 3.1)
+      message(
+          WARNING
+          "Can't use INTERFACE_SOURCES properties. "
+          "Please update CMake to version 3.1+ or add source manually: "
+          "\${QT_ROOT}/src/static_qt_QMngPlugin.cpp"
+      )
+    else()
+      set_target_properties(
+          Qt5::QMngPlugin
+          PROPERTIES
+          INTERFACE_SOURCES
+          "${_qt5Gui_install_prefix}/src/static_qt_QMngPlugin.cpp"
+      )
+    endif()
+  endif()
+endif()
---
+++ scripts/static_qt_QMngPlugin.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2015, Ruslan Baratov
+// All rights reserved.
+
+#include <QtPlugin>
+
+// http://doc.qt.io/qt-5/qtglobal.html
+
+#if defined(Q_OS_IOS)
+  Q_IMPORT_PLUGIN(QMngPlugin);
+#elif defined(Q_OS_DARWIN)
+  // TODO
+#elif defined(Q_OS_LINUX)
+  // TODO
+#elif defined(Q_OS_WIN)
+  // TODO
+#endif
