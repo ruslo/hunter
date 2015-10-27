6f9c266609c88341a2502878ed722b4c0ba71c04
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-10-27 19:46:28 +0600

    Qt: add static_qt_QICNSPlugin.cpp

--- scripts/Qt5Gui_ZZZ_HunterPlugin.cmake
+++ scripts/Qt5Gui_ZZZ_HunterPlugin.cmake
@@ -155,3 +155,26 @@ if(TARGET Qt5::QDDSPlugin)
     endif()
   endif()
 endif()
+
+if(TARGET Qt5::QICNSPlugin)
+  # QICNSPlugin is not always installed (?),
+  # for simplicity check existence of target
+
+  if(_qt5_is_static)
+    if(CMAKE_VERSION VERSION_LESS 3.1)
+      message(
+          WARNING
+          "Can't use INTERFACE_SOURCES properties. "
+          "Please update CMake to version 3.1+ or add source manually: "
+          "\${QT_ROOT}/src/static_qt_QICNSPlugin.cpp"
+      )
+    else()
+      set_target_properties(
+          Qt5::QICNSPlugin
+          PROPERTIES
+          INTERFACE_SOURCES
+          "${_qt5Gui_install_prefix}/src/static_qt_QICNSPlugin.cpp"
+      )
+    endif()
+  endif()
+endif()
