8a5c2b552c508076a37af7450f852b95d798f37b
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-10-28 19:18:40 +0600

    QtCMakeExtra 1.0.1

--- cmake/projects/QtCMakeExtra/hunter.cmake
+++ cmake/projects/QtCMakeExtra/hunter.cmake
@@ -23,6 +23,17 @@ hunter_add_version(
     8657b0878e22a17c046917517dc149657dac3d75
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    QtCMakeExtra
+    VERSION
+    "1.0.1"
+    URL
+    "https://github.com/hunter-packages/QtCMakeExtra/archive/v1.0.1.tar.gz"
+    SHA1
+    cb76d1d7156bf2f79afaef87ffb4502b2fcc1a6e
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(QtCMakeExtra)
 hunter_download(PACKAGE_NAME QtCMakeExtra)
