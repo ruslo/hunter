2da8f8d7d4b2db8b224f07578c04ee055f6a2730
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-12-07 06:53:26 +0700

    Remove tabs and trailing whitespaces

--- cmake/projects/half/hunter.cmake
+++ cmake/projects/half/hunter.cmake
@@ -15,11 +15,11 @@ hunter_add_version(
     PACKAGE_NAME
     half
     VERSION
-	"1.1.0-p0"
+    "1.1.0-p0"
     URL
-	"https://github.com/headupinclouds/half/archive/v1.1.0-p0.tar.gz"
+    "https://github.com/headupinclouds/half/archive/v1.1.0-p0.tar.gz"
     SHA1
-	ad46279b4baf5e819881e66303bb20f88d6df916  
+    ad46279b4baf5e819881e66303bb20f88d6df916
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
