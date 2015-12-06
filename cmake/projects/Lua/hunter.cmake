082a5f96a673be7a3854f6179d20ed29e21b2722
Denis A. Kerzhemanov <kerjemanov@yandex.ru>
2015-12-06 19:39:20 +0300

    Added hunter_configuration_types Added forgotten include for hunter_cacheable

--- cmake/projects/Lua/hunter.cmake
+++ cmake/projects/Lua/hunter.cmake
@@ -1,3 +1,5 @@
+# Added by Denis Kerzhemanov, 2015
+
 if(DEFINED HUNTER_CMAKE_PROJECTS_LUA_HUNTER_CMAKE_)
   return()
 else()
@@ -7,6 +9,8 @@ endif()
 include(hunter_add_version)
 include(hunter_download)
 include(hunter_pick_scheme)
+include(hunter_cacheable)
+include(hunter_configuration_types)
 
 hunter_add_version(
     PACKAGE_NAME
@@ -19,7 +23,7 @@ hunter_add_version(
     5395f8637c14b41f950b28f2c1dba65f30fa7030
 )
 
+hunter_configuration_types(Lua CONFIGURATION_TYPES Release)
 hunter_cacheable(Lua)
-
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_download(PACKAGE_NAME Lua)
