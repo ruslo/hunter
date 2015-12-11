9f112ed772410bc2bfa3d3dc9c585b6a256181c5
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-12-11 22:17:50 +0700

    nlohmann-json: 1.0.0-rc1-hunter-3

--- cmake/projects/nlohmann-json/hunter.cmake
+++ cmake/projects/nlohmann-json/hunter.cmake
@@ -16,5 +16,14 @@ hunter_add_version(
     SHA1 2db824ca17678c8918575bbab982e6ae93283433 
 )
 
+hunter_add_version(
+    PACKAGE_NAME nlohmann-json
+    VERSION "1.0.0-rc1-hunter-3"
+    URL "https://github.com/hunter-packages/json/archive/v1.0.0-rc1-hunter-3.tar.gz"
+    SHA1 4f0300ebb08b28ac6bca0f947ffd6afde64386ef
+)
+
+hunter_cmake_args(nlohmann-json CMAKE_ARGS JSON_BUILD_TESTS=OFF)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_download(PACKAGE_NAME nlohmann-json)
