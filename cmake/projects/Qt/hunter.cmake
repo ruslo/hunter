3114edf629b0435ade3102053c9256a26d745e1a
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-12-18 22:18:02 +0700

    Add Qt's experimental (second version)

--- cmake/projects/Qt/hunter.cmake
+++ cmake/projects/Qt/hunter.cmake
@@ -91,6 +91,17 @@ hunter_add_version(
     PACKAGE_NAME
     Qt
     VERSION
+    "5.5.1-cvpixelbuffer-2-p0"
+    URL
+    "https://github.com/hunter-packages/Qt/releases/download/v5.5.1-cvpixelbuffer-2-p0/hunter-5.5.1-cvpixelbuffer-2.7z"
+    SHA1
+    4efb6c9c37db1846ec70f5fdcf2c719087451075
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    Qt
+    VERSION
     "5.6.0-alpha"
     URL
     "http://download.qt.io/development_releases/qt/5.6/5.6.0-alpha/single/qt-everywhere-opensource-src-5.6.0-alpha.7z"
