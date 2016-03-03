bcc5a1ba1a4940a74ca1d0cb81b9f197bab10ca8
Antal Tátrai <antal.tatrai@gmail.com>
2016-03-03 10:43:35 +0100

    ZeroMQ now can be used as CMake package

--- cmake/projects/ZeroMQ/hunter.cmake
+++ cmake/projects/ZeroMQ/hunter.cmake
@@ -14,9 +14,9 @@ hunter_add_version(
   VERSION
   "4.1.4"
   URL
-  "https://github.com/zeromq/zeromq4-1/archive/v4.1.4.tar.gz"
+  "https://github.com/hunter-packages/zeromq4-1/archive/v4.1.4-hunter.tar.gz"
   SHA1
-  840b24394e68450f46178c16878b0f9dc163e406  
+  aeb0650b1d1b2e5f7c9da81d4e294716eb16e91e
   )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake) 
