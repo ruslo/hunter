de2d6b12d99fb98250971287c51fbeaaa55fe3b1
Antal Tátrai <antal.tatrai@gmail.com>
2016-03-04 16:09:28 +0100

    Static library knows its dependencies

--- cmake/projects/ZeroMQ/hunter.cmake
+++ cmake/projects/ZeroMQ/hunter.cmake
@@ -14,9 +14,9 @@ hunter_add_version(
   VERSION
   "4.1.4"
   URL
-  "https://github.com/hunter-packages/zeromq4-1/archive/v4.1.4-hunter.tar.gz"
+  "https://github.com/hunter-packages/zeromq4-1/archive/v4.1.4-hunter-p1.tar.gz"
   SHA1
-  aeb0650b1d1b2e5f7c9da81d4e294716eb16e91e
+  be57db45f4f998c04d1804132b1302f923405fa4
   )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake) 
