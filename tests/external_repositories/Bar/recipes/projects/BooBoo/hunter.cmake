f3ab6f55fc2c69438e621d7bdc7173fe6a698635
Mayeul d'Avezac <mdavezac@gmail.com>
2016-04-11 17:02:48 +0100

    Adds test for external repos

---
+++ tests/external_repositories/Bar/recipes/projects/BooBoo/hunter.cmake
@@ -0,0 +1,36 @@
+# Copyright (c) 2015, David Hirvonen
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+# Load used modules
+include(hunter_add_version)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+include(hunter_cacheable)
+
+hunter_cacheable(BooBoo)
+
+# List of versions here...
+hunter_add_version(
+    PACKAGE_NAME
+    BooBoo
+    VERSION
+    "1.2"
+    URL
+    "https://bitbucket.org/eigen/eigen/get/3.2.4.tar.gz"
+    SHA1
+    b826f665bdfe31784795eef79c75540db71ab702
+)
+
+hunter_cmake_args(BooBoo CMAKE_ARGS EIGEN_ENABLE_TESTING=OFF)
+
+# Pick a download scheme
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+# Download package.
+# Two versions of library will be build:
+#     * libexample_A.a
+#     * libexample_Ad.a
+hunter_download(PACKAGE_NAME BooBoo)
