8be8e09082edd6dd83f69c6cf47882b4c1372807
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-04-12 18:51:39 +0700

    Qt 5.5.1-cvpixelbuffer-2-p4

--- cmake/projects/Qt/hunter.cmake
+++ cmake/projects/Qt/hunter.cmake
@@ -142,6 +142,17 @@ hunter_add_version(
     PACKAGE_NAME
     Qt
     VERSION
+    "5.5.1-cvpixelbuffer-2-p4"
+    URL
+    "https://github.com/hunter-packages/Qt/releases/download/v5.5.1-cvpixelbuffer-2-p4/hunter-5.5.1-cvpixelbuffer-2.7z"
+    SHA1
+    25b54fb50292f28b76d0631a9e5c791e01bf3c45
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    Qt
+    VERSION
     "5.6.0-alpha"
     URL
     "http://download.qt.io/development_releases/qt/5.6/5.6.0-alpha/single/qt-everywhere-opensource-src-5.6.0-alpha.7z"
