76edda9387391f7089c24f6363a1503dda3078ed
Mayeul d'Avezac <mdavezac@gmail.com>
2016-04-17 19:52:55 +0100

    Formatting more in line with hunter standards

--- cmake/modules/hunter_add_repository.cmake
+++ cmake/modules/hunter_add_repository.cmake
@@ -1,5 +1,6 @@
 include(CMakeParseArguments) # cmake_parse_arguments
 include(hunter_internal_error)
+include(hunter_fatal_error)
 include(hunter_test_string_not_empty)
 include(hunter_status_debug)
 include(hunter_status_print)
@@ -7,7 +8,10 @@ include(hunter_install_repository)
 
 function(hunter_add_repository)
   if(PROJECT_NAME OR NOT HUNTER_GATE_ROOT)
-    message(FATAL_ERROR "hunter_add_repository must be called after HunterGate and before project")
+    hunter_fatal_error(
+        "hunter_add_repository must be called *after* HunterGate"
+        "and *before* project(...)"
+    )
   endif()
   cmake_parse_arguments(har "" "URL;SHA1" "" "${ARGV}")
   hunter_test_string_not_empty("${har_URL}")
@@ -27,30 +31,51 @@ function(hunter_add_repository)
   file(READ "${rootdir}/Done" actual_sha1)
   string(STRIP "${actual_sha1}" actual_sha1)
   if(NOT "${har_SHA1}" STREQUAL "${actual_sha1}")
-    hunter_internal_error("SHA1 of repo does not match ${har_SHA1} vs ${actual_sha1}")
+    hunter_internal_error(
+        "SHA1 of repo does not match ${har_SHA1} vs ${actual_sha1}"
+    )
   endif()
 
   include("${rootdir}/Unpacked/init.cmake")
   if(NOT HUNTER_CURRENT_REPOSITORY_NAME)
-    hunter_internal_error("Repository (${url}) did not call hunter_init_repository")
+    hunter_internal_error(
+        "Repository (${url}) did not call hunter_init_repository"
+    )
   endif()
   if(NOT HUNTER_CURRENT_REPOSITORY_VERSION)
-    hunter_internal_error("Repository (${url}) did not call hunter_init_repository")
+    hunter_internal_error(
+        "Repository (${url}) did not call hunter_init_repository"
+    )
   endif()
 
-  set(reponame "${HUNTER_CURRENT_REPOSITORY_NAME}-${HUNTER_CURRENT_REPOSITORY_VERSION}")
+  set(reponame
+      "${HUNTER_CURRENT_REPOSITORY_NAME}-${HUNTER_CURRENT_REPOSITORY_VERSION}"
+  )
   list(APPEND HUNTER_REPOSITORIES "${reponame}")
   set(HUNTER_REPOSITORIES ${HUNTER_REPOSITORIES} PARENT_SCOPE)
   set(HUNTER_REPOSITORY_${reponame}_DIRECTORY "${rootdir}/Unpacked")
-  set(HUNTER_REPOSITORY_${reponame}_DIRECTORY "${rootdir}/Unpacked" PARENT_SCOPE)
+  set(
+      HUNTER_REPOSITORY_${reponame}_DIRECTORY "${rootdir}/Unpacked"
+      PARENT_SCOPE
+  )
   set(HUNTER_REPOSITORY_${reponame}_URL "${har_URL}" PARENT_SCOPE)
   set(HUNTER_REPOSITORY_${reponame}_SHA1 "${har_SHA1}" PARENT_SCOPE)
-  set(HUNTER_REPOSITORY_${reponame}_NAME "${HUNTER_CURRENT_REPOSITORY_NAME}" PARENT_SCOPE)
+  set(
+      HUNTER_REPOSITORY_${reponame}_NAME "${HUNTER_CURRENT_REPOSITORY_NAME}"
+      PARENT_SCOPE
+  )
   set(HUNTER_REPOSITORY_${reponame}_NAME "${HUNTER_CURRENT_REPOSITORY_NAME}")
-  set(HUNTER_REPOSITORY_${reponame}_VERSION "${HUNTER_CURRENT_REPOSITORY_VERSION}" PARENT_SCOPE)
+  set(
+      HUNTER_REPOSITORY_${reponame}_VERSION
+      "${HUNTER_CURRENT_REPOSITORY_VERSION}" PARENT_SCOPE
+  )
   list(APPEND HUNTER_RECIPE_DIRS "${HUNTER_REPOSITORY_${reponame}_DIRECTORY}")
   set(HUNTER_RECIPE_DIRS ${HUNTER_RECIPE_DIRS} PARENT_SCOPE)
-  # Pass on CMAKE_MODULE_PATH that might have been modified by repo's init script
+  # Pass on CMAKE_MODULE_PATH that might have been modified by repo's init
+  # script
   set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} PARENT_SCOPE)
-  hunter_status_debug("Added repository ${reponame} in ${HUNTER_REPOSITORY_${reponame}_DIRECTORY}")
+  hunter_status_debug(
+      "Added repository ${reponame} in "
+      "${HUNTER_REPOSITORY_${reponame}_DIRECTORY}"
+  )
 endfunction()
--- cmake/modules/hunter_config.cmake
+++ cmake/modules/hunter_config.cmake
@@ -35,20 +35,26 @@ macro(hunter_config)
 
   if(_hunter_REPOSITORY_DIR)
     if(_hunter_REPOSITORY_NAME OR _hunter_REPOSITORY_VERSION)
-      message(FATAL_ERROR "Repository can be specified by directory or name+version, not both")
+      hunter_fatal_error(
+          "Repository can be specified by directory or name+version, not both"
+      )
     endif()
   elseif(_hunter_REPOSITORY_NAME AND NOT _hunter_REPOSITORY_VERSION)
-    message(FATAL_ERROR "Repository name requires repository version as well")
+    hunter_fatal_error(
+        "Repository name requires repository version as well"
+    )
   elseif(_hunter_REPOSITORY_VERSION AND NOT _hunter_REPOSITORY_NAME)
-    message(FATAL_ERROR "Repository version requires repository name as well")
+    hunter_fatal_error(
+        "Repository version requires repository name as well"
+    )
   elseif(_hunter_REPOSITORY_VERSION AND _hunter_REPOSITORY_NAME)
     set(reponame "${_hunter_REPOSITORY_NAME}-${_hunter_REPOSITORY_VERSION}")
     string(REGEX REPLACE "\\." "_" reponame "${reponame}")
     if(NOT "${reponame}" MATCHES "[-a-zA-Z0-9._]*")
-      message(FATAL_ERROR "Repository name not specified correctly")
+      hunter_fatal_error("Repository name not specified correctly")
     endif()
     if(NOT HUNTER_REPOSITORY_${reponame}_DIRECTORY)
-      message(FATAL_ERROR "Could not find repository ${reponame}")
+      hunter_fatal_error("Could not find repository ${reponame}")
     endif()
     set(_hunter_REPOSITORY_DIR "${HUNTER_REPOSITORY_${reponame}_DIRECTORY}")
   endif()
@@ -69,7 +75,10 @@ macro(hunter_config)
         ${_hunter_CONFIGURATION_TYPES}
     )
     if(_hunter_REPOSITORY_DIR)
-      set(HUNTER_${_hunter_current_project}_REPOSITORY ${_hunter_REPOSITORY_DIR})
+      set(
+          HUNTER_${_hunter_current_project}_REPOSITORY
+          ${_hunter_REPOSITORY_DIR}
+      )
     else()
       unset(HUNTER_${_hunter_current_project}_REPOSITORY)
     endif()
