91e34bd21cd77696e98cbce9b6acd80ad54fa064
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-04-22 02:23:57 +0700

    fixesproto 5.0

---
+++ cmake/projects/fixesproto/hunter.cmake
@@ -0,0 +1,28 @@
+# Copyright (c) 2016 Ruslan Baratov
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+# http://www.x.org/releases/individual/proto/
+hunter_add_version(
+    PACKAGE_NAME
+    fixesproto
+    VERSION
+    "5.0"
+    URL
+    "http://www.x.org/releases/individual/proto/fixesproto-5.0.tar.bz2"
+    SHA1
+    ab605af5da8c98c0c2f8b2c578fed7c864ee996a
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_autotools)
+hunter_cacheable(fixesproto)
+hunter_download(
+    PACKAGE_NAME fixesproto
+    PACKAGE_UNRELOCATABLE_TEXT_FILES "lib/pkgconfig/fixesproto.pc"
+)
