fca0c0c2b0a7e0feca2bdac3ad331583ee969a60
Meralis40 <qlaveau@gmail.com>
2016-05-23 19:46:11 +0200

    Change release for libogg
    The git repo for libogg has some corrections for CMake config file

--- cmake/projects/libogg/hunter.cmake
+++ cmake/projects/libogg/hunter.cmake
@@ -6,11 +6,11 @@ hunter_add_version(
 	PACKAGE_NAME
 	libogg
 	VERSION
-	"1.3.2-cmake2"
+	"1.3.2-cmake3"
 	URL
-	"https://github.com/Meralis40/ogg/archive/v1.3.2-cmake2.tar.gz"
+	"https://github.com/Meralis40/ogg/archive/v1.3.2-cmake3.tar.gz"
 	SHA1
-	0b23c94a650c1c1d772bdc06bc7a2b9bd99bf814
+	2640E75997765339BA06BE0E4BFA66D85B70636A
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
