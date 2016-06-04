31f9b831e12434641a8a780a400171a5cf811437
Cyberunner23 <narutoxela@gmail.com>
2016-06-04 09:54:34 -0400

    Updated project to use hunter-packages package and removed previous unpatched versions of SDL2

--- cmake/projects/SDL2/hunter.cmake
+++ cmake/projects/SDL2/hunter.cmake
@@ -13,57 +13,12 @@ hunter_add_version(
     PACKAGE_NAME
     SDL2
     VERSION
-    "2.0.4"
+    "2.0.4-h1"
     URL
-    "https://libsdl.org/release/SDL2-2.0.4.tar.gz"
+    "https://github.com/hunter-packages/SDL2/archive/2.0.4-h1.tar.gz"
     SHA1
-    470a23dfc4b6220c687925ebbe5fe96287f8fb08
+    632a4005aafb25d6fc894a93475f78832a1edd98
 )
-
-hunter_add_version(
-    PACKAGE_NAME
-    SDL2
-    VERSION
-    "2.0.3"
-    URL
-    "https://libsdl.org/release/SDL2-2.0.3.tar.gz"
-    SHA1
-    21c45586a4e94d7622e371340edec5da40d06ecc
-)
-
-hunter_add_version(
-    PACKAGE_NAME
-    SDL2
-    VERSION
-    "2.0.2"
-    URL
-    "https://libsdl.org/release/SDL2-2.0.2.tar.gz"
-    SHA1
-    304c7cd3dddca98724a3e162f232a8a8f6e1ceb3
-)
-
-hunter_add_version(
-    PACKAGE_NAME
-    SDL2
-    VERSION
-    "2.0.1"
-    URL
-    "https://libsdl.org/release/SDL2-2.0.1.tar.gz"
-    SHA1
-    0a3feb5328832f4ffcab0e128ac1aa0c0f9ba224
-)
-
-hunter_add_version(
-    PACKAGE_NAME
-    SDL2
-    VERSION
-    "2.0.0"
-    URL
-    "https://libsdl.org/release/SDL2-2.0.0.tar.gz"
-    SHA1
-    a907eb5203abad6649c1eae0120d96c0a1931350
-)
-
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_download(PACKAGE_NAME SDL2)
