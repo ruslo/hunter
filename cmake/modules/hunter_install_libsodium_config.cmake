af7a3c26d765bdacf3e6c8951ef325948ab2f7f6
Cyberunner23 <narutoxela@gmail.com>
2016-06-11 00:41:43 -0400

    Added package config.

---
+++ cmake/modules/hunter_install_libsodium_config.cmake
@@ -0,0 +1,24 @@
+include(CMakePackageConfigHelpers)
+include(hunter_test_string_not_empty)
+
+function(hunter_install_libsodium_config)
+
+    hunter_test_string_not_empty("${HUNTER_libsodium_VERSION}")
+
+    set(templates "${HUNTER_SELF}/cmake/templates")
+    set(configs "${HUNTER_PACKAGE_INSTALL_PREFIX}/lib/cmake/libsodium")
+
+    # Forward some variables
+    if(BUILD_SHARED_LIBS)
+        set(BUILD_SHARED_LIBS TRUE)
+    endif()
+
+    configure_file(
+            "${templates}/libsodiumConfigVersion.cmake.in"
+            "${configs}/libsodiumConfigVersion.cmake"
+            @ONLY
+    )
+
+    write_basic_package_version_file("${configs}/libsodiumConfig.cmake" VERSION "${HUNTER_libsodium_VERSION}" COMPATIBILITY ExactVersion)
+
+endfunction()
