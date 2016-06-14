57bc8f822e679d3630e8381d72a888a2c8a4c628
Cyberunner23 <narutoxela@gmail.com>
2016-06-14 14:26:09 -0400

    Fixed variable forwarding.

--- cmake/modules/hunter_install_libsodium_config.cmake
+++ cmake/modules/hunter_install_libsodium_config.cmake
@@ -11,9 +11,7 @@ function(hunter_install_libsodium_config)
     set(configs "${HUNTER_PACKAGE_INSTALL_PREFIX}/lib/cmake/libsodium")
 
     # Forward some variables
-    if(BUILD_SHARED_LIBS)
-        set(BUILD_SHARED_LIBS TRUE)
-    endif()
+    set(BUILD_SHARED_LIBS "${BUILD_SHARED_LIBS}")
 
     write_basic_package_version_file("${configs}/libsodiumConfig.cmake" VERSION "${HUNTER_libsodium_VERSION}" COMPATIBILITY ExactVersion)
 
