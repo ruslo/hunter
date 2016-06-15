83b35ac72b59cce39bfad23ac81e04b4d365868f
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-06-15 23:19:50 +0300

    Reduce value

--- cmake/modules/hunter_init_not_found_counter.cmake
+++ cmake/modules/hunter_init_not_found_counter.cmake
@@ -4,7 +4,7 @@
 function(hunter_init_not_found_counter not_found_needed_name not_found_counter_name)
   string(COMPARE EQUAL "${HUNTER_USE_CACHE_SERVERS}" "ONLY" only_servers)
   if(only_servers)
-    set("${not_found_needed_name}" 5 PARENT_SCOPE)
+    set("${not_found_needed_name}" 4 PARENT_SCOPE)
   else()
     set("${not_found_needed_name}" 2 PARENT_SCOPE)
   endif()
