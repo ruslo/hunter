3baf24b0034389215dad2c9e1b04dae913bb6d0e
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-06-23 03:38:14 +0300

    Assimp 3.2-p0

---
+++ cmake/projects/Assimp/hunter.cmake
@@ -0,0 +1,33 @@
+# Copyright (c) 2016, Ruslan Baratov
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    Assimp
+    VERSION
+    3.2-p0
+    URL
+    "https://github.com/hunter-packages/assimp/archive/v3.2-p0.tar.gz"
+    SHA1
+    a3f46eb24bc48cacee9f21b1ea4f4a0e3122b847
+)
+
+hunter_cmake_args(
+    Assimp
+    CMAKE_ARGS
+    ASSIMP_BUILD_ASSIMP_TOOLS=OFF
+    ASSIMP_BUILD_SAMPLES=ON
+    ASSIMP_BUILD_TESTS=ON
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(Assimp)
+hunter_download(PACKAGE_NAME Assimp)
