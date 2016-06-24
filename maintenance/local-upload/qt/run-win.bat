9f41be9918fc6d6a78399a8061cee9bea272a8b7
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-06-24 13:31:12 +0300

    Remove vs-11-2012 toolchain

--- maintenance/local-upload/qt/run-win.bat
+++ maintenance/local-upload/qt/run-win.bat
@@ -23,18 +23,6 @@ set PROJECT_DIR=examples/qt-camera
 REM }
 
 REM {
-set TOOLCHAIN=vs-11-2012
-set PROJECT_DIR=examples/qt-widgets
-.\jenkins.py --verbose --clear-except-download || exit /b 1
-set PROJECT_DIR=examples/qt-qml
-.\jenkins.py --verbose --nocreate || exit /b 1
-set PROJECT_DIR=examples/qt-location
-.\jenkins.py --verbose --nocreate || exit /b 1
-set PROJECT_DIR=examples/qt-camera
-.\jenkins.py --verbose --upload --nocreate || exit /b 1
-REM }
-
-REM {
 set TOOLCHAIN=mingw
 set PROJECT_DIR=examples/qt-widgets
 .\jenkins.py --verbose --clear-except-download || exit /b 1
