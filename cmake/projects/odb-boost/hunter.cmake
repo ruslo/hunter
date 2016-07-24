bcc83b50f21b725c4c486564dfbc2688d487698f
Alexandre Pretyman <alexandre.pretyman@gmail.com>
2016-07-23 23:47:59 -0300

    Package odb-boost: enable cache

--- cmake/projects/odb-boost/hunter.cmake
+++ cmake/projects/odb-boost/hunter.cmake
@@ -4,6 +4,7 @@
 # !!! DO NOT PLACE HEADER GUARDS HERE !!!
 
 include(hunter_add_version)
+include(hunter_cacheable)
 include(hunter_configuration_types)
 include(hunter_pick_scheme)
 include(hunter_download)
@@ -21,5 +22,10 @@ hunter_add_version(
 
 hunter_configuration_types(odb-boost CONFIGURATION_TYPES Release)
 hunter_pick_scheme(DEFAULT url_sha1_odb-boost_autotools)
-hunter_download(PACKAGE_NAME odb-boost)
+hunter_cacheable(odb-boost)
+hunter_download(PACKAGE_NAME odb-boost
+    PACKAGE_UNRELOCATABLE_TEXT_FILES
+    "lib/libodb-boost.la"
+    "lib/pkgconfig/libodb-boost.pc"
+)
 
