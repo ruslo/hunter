9c3cec2343d20fc35e660d5e718223146ff3cbd2
Antal Tátrai <antal.tatrai@gmail.com>
2016-09-06 09:30:07 +0200

    eliminate chrono

--- examples/Boost-thread/foo.cpp
+++ examples/Boost-thread/foo.cpp
@@ -1,6 +1,5 @@
 #include <boost/thread/thread.hpp>
-#include <boost/chrono/duration.hpp>
 
 int main() {
-    boost::this_thread::no_interruption_point::sleep_for(boost::chrono::seconds(2));
+    boost::this_thread::get_id();
 }
