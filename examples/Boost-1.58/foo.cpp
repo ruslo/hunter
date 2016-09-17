dcede06570d0cd808c5cb80dae612f165f5d5152
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-09-17 21:58:46 +0300

    Remove experimental stuff

--- examples/Boost-1.58/foo.cpp
+++
@@ -1,7 +0,0 @@
-#include <boost/filesystem.hpp>
-#include <iostream> // std::cout
-
-int main() {
-  namespace fs = boost::filesystem;
-  std::cout << "Current path: " << fs::current_path() << std::endl;
-}
