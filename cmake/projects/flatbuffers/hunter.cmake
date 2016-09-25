7621fd812b984fd6a02ba6b86e9a33264ae17a5a
Andrew Hundt <ATHundt@gmail.com>
2016-09-25 16:02:11 -0400

    Update flatbuffers hunter.cmake for 1.4.0-p1
    FLATC and FLATLIB parameters set to on because this matches the flatbuffers default

--- cmake/projects/flatbuffers/hunter.cmake
+++ cmake/projects/flatbuffers/hunter.cmake
@@ -13,19 +13,19 @@ hunter_add_version(
     PACKAGE_NAME
     flatbuffers
     VERSION
-    1.3.0-p3
+    1.4.0-p3
     URL
-    "https://github.com/hunter-packages/flatbuffers/archive/v1.3.0-p3.tar.gz"
+    "https://github.com/hunter-packages/flatbuffers/archive/v1.4.0-p1.tar.gz"
     SHA1
-    b031689b876c2abac28c8e22fea68ca6d572d1f5
+    dd97013e6aa60c2626b33091d4523fb6402bf922
 )
 
 hunter_cmake_args(
     flatbuffers
     CMAKE_ARGS
-        FLATBUFFERS_BUILD_FLATC=OFF
+        FLATBUFFERS_BUILD_FLATC=ON
         FLATBUFFERS_BUILD_FLATHASH=OFF
-        FLATBUFFERS_BUILD_FLATLIB=OFF
+        FLATBUFFERS_BUILD_FLATLIB=ON
         FLATBUFFERS_BUILD_TESTS=OFF
 )
 
