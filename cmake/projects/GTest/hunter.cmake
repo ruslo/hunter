5166445f4a8e838bcb1833ce23148f0a4a9497ed
Knitschi <knitschi@freenet.de>
2016-10-21 14:46:28 +0200

    -Added the new version 1.8.0-hunter-p4 of the GTest Package.\n-Modified the GTest example to also use GMock.

--- cmake/projects/GTest/hunter.cmake
+++ cmake/projects/GTest/hunter.cmake
@@ -151,6 +151,15 @@ hunter_add_version(
     93148cb8850abe78b76ed87158fdb6b9c48e38c4
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    GTest
+    VERSION
+    1.8.0-hunter-p4
+    URL https://github.com/hunter-packages/googletest/archive/1.8.0-hunter-p4.tar.gz
+    SHA1 dafd7c28c387c39fb2821484da8a1c60d1ce6470
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(GTest)
 hunter_download(PACKAGE_NAME GTest PACKAGE_INTERNAL_DEPS_ID 1)
--- examples/GTest/foo.cpp
+++ examples/GTest/foo.cpp
@@ -1,5 +1,32 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-TEST(Foo, Boo) {
-  ASSERT_EQ(3, 3);
+using namespace testing;
+
+class MyClass
+{
+public:
+    virtual void doSomething(){}
+};
+
+class MockClass : public MyClass
+{
+public:
+    MOCK_METHOD0(doSomething, void());  // MOCK_METHOD is from gmock.h
+};
+
+TEST(MyTest, demonstrate_clang_tidy_warning)    // TEST is from gtest.h
+{
+    MockClass myMockClass;
+
+    EXPECT_CALL(myMockClass, doSomething()).Times(1);
+    
+    myMockClass.doSomething();
 }
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
