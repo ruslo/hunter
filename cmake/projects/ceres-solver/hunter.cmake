f90a3f322b17fa78e416ce56b335bdce551c3a14
tbaudier <thomas.baudier@creatis.insa-lyon.fr>
2016-10-24 13:19:55 +0200

    Add ceres-solver

---
+++ cmake/projects/ceres-solver/hunter.cmake
@@ -0,0 +1,29 @@
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+# Load used modules
+include(hunter_add_version)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+# List of versions here...
+hunter_add_version(
+    PACKAGE_NAME
+    ceres-solver
+    VERSION
+    "1.12.0"
+    URL
+    "https://github.com/hunter-packages/ceres-solver/archive/hunter.zip"
+    SHA1
+    8554129d92cdddaead42274a47322709f82943e0
+)
+
+# Probably more versions for real packages...
+
+# Pick a download scheme
+hunter_pick_scheme(DEFAULT url_sha1_cmake) # use scheme for cmake projects
+
+# Download package.
+# Two versions of library will be build by default:
+#     * libexample_A.a
+#     * libexample_Ad.a
+hunter_download(PACKAGE_NAME ceres-solver)
