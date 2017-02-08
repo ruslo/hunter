1b5aa558f05cb7d349c7927fc169eaa90a55a242
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2017-02-08 12:47:02 +0000

    Adds F2C

---
+++ cmake/find/FindF2C.cmake
@@ -0,0 +1,11 @@
+# Copyright (c) 2015-2016, University College London
+# All rights reserved.
+
+find_path(F2C_INCLUDE_DIR NAMES f2c.h)
+find_library(F2C_LIBRARY NAMES libf2c.a f2c)
+
+set(F2C_INCLUDE_DIRS ${F2C_INCLUDE_DIR})
+set(F2C_LIBRARIES ${F2C_LIBRARY})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(F2C REQUIRED_VARS F2C_INCLUDE_DIR F2C_LIBRARY)
