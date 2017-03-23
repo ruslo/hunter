104a6939466226a56a4725f212d3185ead38483c
Aaditya Kalsi <akalsi.dev@gmail.com>
2017-03-22 21:02:30 -0400

    Update sparsehash download and SHA1

--- cmake/projects/sparsehash/hunter.cmake
+++ cmake/projects/sparsehash/hunter.cmake
@@ -10,7 +10,6 @@
 include(hunter_add_version)
 include(hunter_download)
 include(hunter_pick_scheme)
-include(hunter_report_broken_package)
 
 # Version list
 hunter_add_version(
@@ -19,17 +18,13 @@ hunter_add_version(
   VERSION
   "2.0.2"
   URL
-  https://github.com/aadityakalsi/sparsehash/archive/sparsehash-2.0.2-cmake.tar.gz
+  https://github.com/aadityakalsi/sparsehash/archive/v2.0.2-hunter.tar.gz
   SHA1
-  8fa7190fa2ad43218f91717be0bf151667773263
+  24e725a008d065ec84c8f2a2bdbb5708dab162ac
 )
 
 # Default CMake scheme
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
-
-hunter_report_broken_package(
-    "'sparsehash' package is destructive (https://github.com/ruslo/hunter/issues/695)"
-)
 
 # This is header-only, so only headers are needed
 hunter_download(PACKAGE_NAME sparsehash)
