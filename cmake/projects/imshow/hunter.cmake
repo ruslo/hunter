c39fa76b0c3e78c03e7e239162e5013a35885c8f
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-03-27 12:23:50 +0800

    imshow 1.0.0-p0

--- cmake/projects/imshow/hunter.cmake
+++ cmake/projects/imshow/hunter.cmake
@@ -21,6 +21,17 @@ hunter_add_version(
     3b01b2cdd59d8a90da0df45888e96175da009ef5
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    imshow
+    VERSION
+    "1.0.0-p0"
+    URL
+    "https://github.com/hunter-packages/imshow/archive/v1.0.0-p0.tar.gz"
+    SHA1
+    196be66bb2bf62747e638b87444b88824ca51a18
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(imshow)
 hunter_download(PACKAGE_NAME imshow)
