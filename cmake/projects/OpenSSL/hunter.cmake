2bde3da15383fb6e33c1184556affa083675b679
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-03-27 14:22:12 +0800

    OpenSSL 1.0.2k

--- cmake/projects/OpenSSL/hunter.cmake
+++ cmake/projects/OpenSSL/hunter.cmake
@@ -45,6 +45,17 @@ hunter_add_version(
     PACKAGE_NAME
     OpenSSL
     VERSION
+    "1.0.2k"
+    URL
+    "https://github.com/openssl/openssl/archive/OpenSSL_1_0_2k.tar.gz"
+    SHA1
+    462944eff7b045d950deaaa86798190cbdc5278a
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    OpenSSL
+    VERSION
     "1.0.2j"
     URL
     "https://github.com/openssl/openssl/archive/OpenSSL_1_0_2j.tar.gz"
