0b0ba29feb0fe9005329e91fb7ce86f401020835
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-03-28 13:27:49 +0800

    boost-pba 1.0.0-p0

--- cmake/projects/boost-pba/hunter.cmake
+++ cmake/projects/boost-pba/hunter.cmake
@@ -16,9 +16,20 @@ hunter_add_version(
     VERSION
     "1.0.0"
     URL
-    "https://github.com/hunter-packages/boost-pba/archive/v1.0.0.tar.gz"    
+    "https://github.com/hunter-packages/boost-pba/archive/v1.0.0.tar.gz"
     SHA1
     65b28b923cf0330860b36d568bf4a98503fb0455
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    boost-pba
+    VERSION
+    "1.0.0-p0"
+    URL
+    "https://github.com/hunter-packages/boost-pba/archive/v1.0.0-p0.tar.gz"
+    SHA1
+    c4dd316edbb6839c5be2a5e1a9decd3bdfd848d0
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
