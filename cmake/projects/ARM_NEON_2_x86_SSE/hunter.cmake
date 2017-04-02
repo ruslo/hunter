dd13f98c1da1f061264332cd8a9b20ed9196704f
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-04-02 13:57:15 +0800

    ARM_NEON_2_x86_SSE 1.0.0-p0

--- cmake/projects/ARM_NEON_2_x86_SSE/hunter.cmake
+++ cmake/projects/ARM_NEON_2_x86_SSE/hunter.cmake
@@ -20,6 +20,17 @@ hunter_add_version(
     8067ea1b7aa9ec991db5989d7eda6ed7293d0fda
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    ARM_NEON_2_x86_SSE
+    VERSION
+    1.0.0-p0
+    URL
+    "https://github.com/hunter-packages/ARM_NEON_2_x86_SSE/archive/v1.0.0-p0.tar.gz"
+    SHA1
+    bf97b9ae78060b4dc9aa2afd3a2a1d577b405b2c
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(ARM_NEON_2_x86_SSE)
 hunter_download(PACKAGE_NAME ARM_NEON_2_x86_SSE)
