4b2bc67fde697a8c58bca993112b6ea58ea673f7
Paweł Bylica <chfast@gmail.com>
2017-04-02 20:58:11 +0200

    Refactor hunter_get_lang_standard_flag()
    Apply suggestions from the review https://github.com/ruslo/hunter/pull/715.

--- cmake/modules/hunter_get_lang_standard_flag.cmake
+++ cmake/modules/hunter_get_lang_standard_flag.cmake
@@ -9,32 +9,53 @@ function(hunter_get_lang_standard_flag LANG OUTPUT)
   # Find the <lang> standard flag.
   # This maps the logic in the CMake code:
   # https://github.com/Kitware/CMake/blob/3bccdd89c88864839a0c8d4ea56bd069c90fa02b/Source/cmLocalGenerator.cxx#L1433-L1467
-  set(flag "")
-  set(std "${CMAKE_${LANG}_STANDARD}")
-  if(std)
-    set(ext "EXTENSION")  # By default extensions are assumed On.
-    if(DEFINED CMAKE_${LANG}_EXTENSIONS AND NOT CMAKE_${LANG}_EXTENSIONS)
-      set(ext "STANDARD")
-    endif()
 
-    set(standards "${${LANG}_standards}")
-    list(FIND standards "${std}" begin)
-    if(NOT "${begin}" EQUAL -1)
-      list(LENGTH standards end)
-      math(EXPR end "${end} - 1")
-      foreach(idx RANGE ${begin} ${end})
-        list(GET standards ${idx} std)
-        set(option_name "CMAKE_${LANG}${std}_${ext}_COMPILE_OPTION")
-        set(flag "${${option_name}}")
-        string(COMPARE NOTEQUAL "${flag}" "" has_flag)
-        if(has_flag OR CMAKE_${LANG}_STANDARD_REQUIRED)
-          # Break if flag found or standard is required and we don't want to
-          # continue checking older standards.
-          break()
-        endif()
-      endforeach()
-    endif()
+  hunter_status_debug("CMAKE_${LANG}_STANDARD_DEFAULT: ${CMAKE_${LANG}_STANDARD_DEFAULT}")
+  hunter_status_debug("CMAKE_${LANG}_STANDARD: ${CMAKE_${LANG}_STANDARD}")
+  hunter_status_debug("CMAKE_${LANG}_EXTENSIONS: ${CMAKE_${LANG}_EXTENSIONS}")
+  hunter_status_debug("CMAKE_${LANG}_STANDARD_REQUIRED: ${CMAKE_${LANG}_STANDARD_REQUIRED}")
+
+  set("${OUTPUT}" "" PARENT_SCOPE)  # Reset output in case of quick return.
+
+  string(COMPARE EQUAL "${CMAKE_${LANG}_STANDARD_DEFAULT}" "" no_default)
+  if(no_default)
+    # This compiler has no notion of language standard levels.
+    return()
   endif()
-  hunter_status_debug("${LANG} std flag: '${flag}'")
-  set(${OUTPUT} "${flag}" PARENT_SCOPE)
+
+  set(standard "${CMAKE_${LANG}_STANDARD}")
+  string(COMPARE EQUAL "${standard}" "" no_standard)
+  if(no_standard)
+    # The standard not defined by user.
+    return()
+  endif()
+
+  set(flag "")
+  set(ext "EXTENSION")  # By default extensions are assumed On.
+  if(DEFINED CMAKE_${LANG}_EXTENSIONS AND NOT CMAKE_${LANG}_EXTENSIONS)
+    set(ext "STANDARD")
+  endif()
+
+  set(standards "${${LANG}_standards}")
+  list(FIND standards "${standard}" begin)
+  if("${begin}" EQUAL "-1")
+    hunter_internal_error("${LANG} standard ${standard} not known")
+    return()
+  endif()
+  list(LENGTH standards end)
+  math(EXPR end "${end} - 1")
+  foreach(idx RANGE ${begin} ${end})
+    list(GET standards ${idx} standard)
+    set(option_name "CMAKE_${LANG}${standard}_${ext}_COMPILE_OPTION")
+    set(flag "${${option_name}}")
+    hunter_status_debug("${option_name}: '${flag}'")
+    string(COMPARE NOTEQUAL "${flag}" "" has_flag)
+    if(has_flag OR CMAKE_${LANG}_STANDARD_REQUIRED)
+      # Break if flag found or standard is required and we don't want to
+      # continue checking older standards.
+      break()
+    endif()
+  endforeach()
+  hunter_status_debug("hunter_get_lang_standard_flag(${LANG}): '${flag}'")
+  set("${OUTPUT}" "${flag}" PARENT_SCOPE)
 endfunction()
