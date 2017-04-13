156aa14b2506ba7a80805cca9758ee86e6a2c2b3
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-04-13 21:06:38 +0800

    mtplz 0.1-p3

--- cmake/projects/mtplz/hunter.cmake
+++ cmake/projects/mtplz/hunter.cmake
@@ -20,7 +20,24 @@ hunter_add_version(
     3323b6a97afefa0cc008785caa37b43681c9589e
 )
 
-hunter_cmake_args(mtplz CMAKE_ARGS BUILD_TESTING=OFF)
+hunter_add_version(
+    PACKAGE_NAME
+    mtplz
+    VERSION
+    0.1-p3
+    URL
+    "https://github.com/hunter-packages/mtplz/archive/v0.1-p3.tar.gz"
+    SHA1
+    22a29dbe58a86e4e91e1abeb1ab4c67cf3ceca79
+)
+
+hunter_cmake_args(
+    mtplz
+    CMAKE_ARGS
+    BUILD_TESTING=OFF
+    MTPLZ_BUILD_EXE=OFF
+    MTPLZ_BUILD_KENLM_INTERPOLATE=OFF
+)
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(mtplz)
