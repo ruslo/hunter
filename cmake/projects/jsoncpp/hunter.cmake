b0c1b04a42c1e1154b0e14a4abf927ce1fe3a72b
Paweł Bylica <chfast@gmail.com>
2017-04-22 13:03:37 +0200

    jsoncpp: reorder versions from newest

--- cmake/projects/jsoncpp/hunter.cmake
+++ cmake/projects/jsoncpp/hunter.cmake
@@ -8,17 +8,7 @@ include(hunter_download)
 include(hunter_pick_scheme)
 include(hunter_cmake_args)
 
-# List of versions here...
-hunter_add_version(
-    PACKAGE_NAME
-    jsoncpp
-    VERSION
-    "0.7.0"
-    URL
-    "https://github.com/open-source-parsers/jsoncpp/archive/0.7.0.tar.gz"
-    SHA1
-    4fcb0e3275a1391856fc6ae21e36dce866b19393
-)
+# List of versions:
 
 hunter_add_version(
     PACKAGE_NAME
@@ -31,7 +21,16 @@ hunter_add_version(
     7bbb47e25b3aa7c4c8b579ca46b32d55f32cb46e
 )
 
-# Probably more versions for real packages...
+hunter_add_version(
+    PACKAGE_NAME
+    jsoncpp
+    VERSION
+    "0.7.0"
+    URL
+    "https://github.com/open-source-parsers/jsoncpp/archive/0.7.0.tar.gz"
+    SHA1
+    4fcb0e3275a1391856fc6ae21e36dce866b19393
+)
 
 # Pick a download scheme
 hunter_pick_scheme(DEFAULT url_sha1_cmake) # use scheme for cmake projects
