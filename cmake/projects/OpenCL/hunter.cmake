dc51fefee11444bde0e5b0698d253b1b7479441e
Paweł Bylica <chfast@gmail.com>
2017-04-28 17:15:31 +0200

    OpenCL: add version 2.1-p3

--- cmake/projects/OpenCL/hunter.cmake
+++ cmake/projects/OpenCL/hunter.cmake
@@ -3,10 +3,22 @@
 # Load used modules
 include(hunter_add_version)
 include(hunter_cacheable)
+include(hunter_cmake_args)
 include(hunter_download)
 include(hunter_pick_scheme)
 
 # List of versions here...
+hunter_add_version(
+    PACKAGE_NAME
+    OpenCL
+    VERSION
+    "2.1-p3"
+    URL
+    "https://github.com/hunter-packages/OpenCL-ICD-Loader/archive/2.1-p3.tar.gz"
+    SHA1
+    be6a6c575088103f1b87c5202dc0f69e49b3764f
+)
+
 hunter_add_version(
     PACKAGE_NAME
     OpenCL
@@ -16,6 +28,13 @@ hunter_add_version(
     "https://github.com/hunter-packages/OpenCL-ICD-Loader/archive/2.1-p0.tar.gz"
     SHA1
     1cb4fed5bc4d0e1583c07e02f43daccab12eb99a
+)
+
+
+hunter_cmake_args(
+    OpenCL
+    CMAKE_ARGS
+        OPENCL_TESTS=OFF
 )
 
 # Pick a download scheme
