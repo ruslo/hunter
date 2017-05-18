ff03f0eea39fbe9d9ab12f5a8106c13224f79c0e
Viktor Kirilov <vik.kirilov@gmail.com>
2017-05-18 11:10:34 +0300

    [doctest] updating to version 1.2.0
    also using the original repository instead of a fork

--- cmake/projects/doctest/hunter.cmake
+++ cmake/projects/doctest/hunter.cmake
@@ -14,16 +14,15 @@ hunter_add_version(
     PACKAGE_NAME
     doctest
     VERSION
-    "1.1.4-hunter-1"
+    "1.2.0"
     URL
-    "https://github.com/piribes/doctest/archive/1.1.4-hunter-1.tar.gz"
+    "https://github.com/onqtam/doctest/archive/1.2.0.tar.gz"
     SHA1
-    684580f4cf1f40ca7750bea90facc4564a807c2a
+    53f87a422c7cc842d9b3e06d5a7eb7e35307e6ac
 )
 
 hunter_cmake_args(doctest CMAKE_ARGS
-    DOCTEST_SKIP_EXAMPLES=ON
-    DOCTEST_SKIP_COVERAGE=ON
+    DOCTEST_WITH_TESTS=OFF
 )
 
 # Pick a download scheme
