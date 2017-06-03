5a6d6b2a266a098902c83cb0ea8f42a2aa2f3c3a
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-06-03 18:23:33 +0300

    OpenNMTTokenizer 0.2.0-p1

--- cmake/projects/OpenNMTTokenizer/hunter.cmake
+++ cmake/projects/OpenNMTTokenizer/hunter.cmake
@@ -20,6 +20,17 @@ hunter_add_version(
     8d6025d92014586c5ff5a831c13b9238e6c07da8
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    OpenNMTTokenizer
+    VERSION
+    0.2.0-p1
+    URL
+    "https://github.com/hunter-packages/OpenNMT-Tokenizer/archive/v0.2.0-p1.tar.gz"
+    SHA1
+    33c75c6d22a79a0410b28a518b57a5b257762f1f
+)
+
 hunter_cmake_args(OpenNMTTokenizer CMAKE_ARGS LIB_ONLY=YES)
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
