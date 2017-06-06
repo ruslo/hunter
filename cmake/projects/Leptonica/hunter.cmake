9fabbb014ddeb4ab7bcaaace0d8be69b858a7295
Sacha Refshauge <srefshauge@imagus.com.au>
2017-06-06 11:06:16 +1000

    Leptonica: Fix MSVC 2015

--- cmake/projects/Leptonica/hunter.cmake
+++ cmake/projects/Leptonica/hunter.cmake
@@ -12,11 +12,11 @@ hunter_add_version(
     PACKAGE_NAME
     Leptonica
     VERSION
-    "1.74.2-p3"
+    "1.74.2-p4"
     URL
-    "https://github.com/hunter-packages/leptonica/archive/v1.74.2-p3.tar.gz"
+    "https://github.com/hunter-packages/leptonica/archive/v1.74.2-p4.tar.gz"
     SHA1
-    ce7e50033e807c1145c858390315ad32844ce16b
+    b317631496a683759565fc925e5125fddf85466a
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
