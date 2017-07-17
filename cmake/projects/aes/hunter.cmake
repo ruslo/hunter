d5e7d0995dacf5e79e5a3725a5653a9c00b39368
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-07-17 12:51:10 +0300

    aes 0.0.1-p1

--- cmake/projects/aes/hunter.cmake
+++ cmake/projects/aes/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     49ae29df3b0e0619c1db7e7696795c956386f71b
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    aes
+    VERSION
+    0.0.1-p1
+    URL
+    "https://github.com/hunter-packages/aes/archive/v0.0.1-p1.tar.gz"
+    SHA1
+    5ec961526679246b34332afd1d83c015c377bd79
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(aes)
 hunter_download(PACKAGE_NAME aes)
