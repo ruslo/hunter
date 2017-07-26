c7cc4d26c442511b6dd6c701692d9b60317ba364
dan-42 <daniel@duerrenbuehl.de>
2017-07-26 18:46:46 +0200

    FEATURE: add support for nlohman_json V2.1.1

--- cmake/projects/nlohmann-json/hunter.cmake
+++ cmake/projects/nlohmann-json/hunter.cmake
@@ -20,7 +20,16 @@ hunter_add_version(
     SHA1 4f0300ebb08b28ac6bca0f947ffd6afde64386ef
 )
 
+hunter_add_version(
+    PACKAGE_NAME nlohmann-json
+    VERSION "2.1.1"
+    URL "https://github.com/nlohmann/json/archive/v2.1.1.tar.gz"
+    SHA1 abee5b79c8a04ece60c496759f4bd002e9cd5077
+)
+
+
 hunter_cmake_args(nlohmann-json CMAKE_ARGS JSON_BUILD_TESTS=OFF)
+hunter_cmake_args(nlohmann-json CMAKE_ARGS JSON_BuildTests=OFF)
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(nlohmann-json)
