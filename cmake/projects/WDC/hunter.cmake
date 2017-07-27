36a509a13e46e2e9a8415f02c8cabbce3d518428
rusdevops <rusdevops@gmail.com>
2017-07-27 13:03:19 +0300

    Update hunter.cmake

--- cmake/projects/WDC/hunter.cmake
+++ cmake/projects/WDC/hunter.cmake
@@ -8,6 +8,13 @@ include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME WDC
+    VERSION "1.0.6"
+    URL "https://github.com/CloudPolis/webdav-client-cpp/archive/v1.0.6.tar.gz"
+    SHA1 db0358658f689cc9118d96acf1d6de6eeb4d9041
+)
+
+hunter_add_version(
+    PACKAGE_NAME WDC
     VERSION "1.0.5"
     URL "https://github.com/CloudPolis/webdav-client-cpp/archive/v1.0.5.tar.gz"
     SHA1 5f0d0cce727d079f3acc822737fc5e9f93aabed2
