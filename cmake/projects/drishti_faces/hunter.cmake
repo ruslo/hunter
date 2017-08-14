8d5c396ae67d7723c0744cb6047a24fa65bc1e31
David Hirvonen <dhirvonen@elucideye.com>
2017-08-14 11:58:32 -0400

    drishti-faces 1.2
    updated ground truth for testing

--- cmake/projects/drishti_faces/hunter.cmake
+++ cmake/projects/drishti_faces/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     2518ff034bd4c2967cbab4fc7a69d30cf0de057a
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    drishti_faces
+    VERSION
+    1.2
+    URL
+    "https://github.com/elucideye/drishti-faces/archive/v1.2.tar.gz"
+    SHA1
+    61cf9e459856a06d2da83f84f43f1a0e43955d7a    
+)  
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(drishti_faces)
 hunter_download(PACKAGE_NAME drishti_faces)
