9b080bcf2477b53e8b00ca3d407efa5488176760
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-08-16 18:48:20 +0300

    intsizeof 2.0.0

--- cmake/projects/intsizeof/hunter.cmake
+++ cmake/projects/intsizeof/hunter.cmake
@@ -1,15 +1,16 @@
-# Copyright (c) 2014, Ruslan Baratov
+# Copyright (c) 2014-2017, Ruslan Baratov
 # All rights reserved.
 
 # !!! DO NOT PLACE HEADER GUARDS HERE !!!
 
 include(hunter_add_version)
+include(hunter_cacheable)
 include(hunter_download)
 include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME
-    IntSizeof
+    intsizeof
     VERSION
     "1.0.0"
     URL
@@ -18,5 +19,17 @@ hunter_add_version(
     9959e0e29c88fe414ba082fb28a722e411682bb3
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    intsizeof
+    VERSION
+    "2.0.0"
+    URL
+    "https://github.com/ruslo/intsizeof/archive/v2.0.0.tar.gz"
+    SHA1
+    35b954c7180bce95f491951a87a49098bb074595
+)
+
+hunter_cacheable(intsizeof)
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
-hunter_download(PACKAGE_NAME IntSizeof)
+hunter_download(PACKAGE_NAME intsizeof)
