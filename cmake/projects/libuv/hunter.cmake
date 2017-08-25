da3fea4d9d01c8676e052c001d751f13861db41c
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-08-25 17:07:17 +0300

    libuv 2.0.0-p0

--- cmake/projects/libuv/hunter.cmake
+++ cmake/projects/libuv/hunter.cmake
@@ -1,14 +1,10 @@
-# libuv
-
 # !!! DO NOT PLACE HEADER GUARDS HERE !!!
 
-# Load used modules
 include(hunter_add_version)
 include(hunter_cacheable)
 include(hunter_download)
 include(hunter_pick_scheme)
 
-# List of versions here...
 hunter_add_version(
   PACKAGE_NAME
   libuv
@@ -20,9 +16,18 @@ hunter_add_version(
   60dee8435f4c4136a2ecb6814a2f302641ef1093
   )
 
-# Pick a download scheme
-hunter_pick_scheme(DEFAULT url_sha1_cmake) # use scheme for cmake projects
+hunter_add_version(
+  PACKAGE_NAME
+  libuv
+  VERSION
+  2.0.0-p0
+  URL
+  "https://github.com/hunter-packages/libuv/archive/v2.0.0-p0.tar.gz"
+  SHA1
+  65a011219de5ad16e2de2df4b07b945a9e67035d
+  )
 
-# Download package.
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
 hunter_cacheable(libuv)
 hunter_download(PACKAGE_NAME libuv)
