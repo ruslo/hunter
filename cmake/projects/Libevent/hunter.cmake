b4a994c7e890a5510680c8b87a814c0529bb1383
Isaac Hier <isaachier@gmail.com>
2017-09-10 12:11:39 -0400

    Update release for Libevent.

--- cmake/projects/Libevent/hunter.cmake
+++ cmake/projects/Libevent/hunter.cmake
@@ -8,8 +8,8 @@ include(hunter_pick_scheme)
 hunter_add_version(
     PACKAGE_NAME Libevent
     VERSION "2.1.8"
-    URL "https://github.com/isaachier/libevent/archive/hunter-2.1.8-p0.tar.gz"
-    SHA1 "04803b553a579b3a69f4778ddc9eb498554f4e29")
+    URL "https://github.com/isaachier/libevent/archive/hunter-2.1.8-p1.tar.gz"
+    SHA1 "743ee49ede4a1cb2a1020e24ee9b1c061255d475")
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(Libevent)
