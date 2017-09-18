97b929dd9b6fa38c5c73bcb41cfe652e13ba0d44
Fabian Winnen <fabian.winnen@gmail.com>
2017-09-18 16:43:52 +0200

    Added updated ZeroMQ 4.2.3 (which is also compatible with Windows UWP)

--- cmake/projects/ZeroMQ/hunter.cmake
+++ cmake/projects/ZeroMQ/hunter.cmake
@@ -28,6 +28,17 @@ hunter_add_version(
   be57db45f4f998c04d1804132b1302f923405fa4
   )
 
+hunter_add_version(
+    PACKAGE_NAME
+    ZeroMQ
+    VERSION
+    "4.2.3"
+    URL
+    "https://github.com/hunter-packages/libzmq/releases/download/v4.2.3-hunter/libzmq-4.2.3-hunter.tar.gz"
+    SHA1
+    ad2f366f208bc1d964e7eab23efcac6e1116fc14
+)
+
 hunter_cmake_args(ZeroMQ CMAKE_ARGS ZMQ_BUILD_TESTS=OFF)
 hunter_pick_scheme(DEFAULT url_sha1_cmake) 
 hunter_cacheable(ZeroMQ)
