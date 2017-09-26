47e7307e89913144993bb99e255511619158fa44
Isaac Hier <ihier@uber.com>
2017-09-26 12:16:57 -0400

    Update gumbo version.

--- cmake/projects/gumbo/hunter.cmake
+++ cmake/projects/gumbo/hunter.cmake
@@ -9,8 +9,8 @@ include(hunter_pick_scheme)
 hunter_add_version(
     PACKAGE_NAME gumbo
     VERSION "0.10.1"
-    URL "https://github.com/isaachier/gumbo-parser/archive/hunter-0.10.1-p1.tar.gz"
-    SHA1 "44c29f792d2129fbd5df55dea529e4b5f2271bce")
+    URL "https://github.com/isaachier/gumbo-parser/archive/hunter-0.10.1-p2.tar.gz"
+    SHA1 "b33e7738e12611ffe2deb6dc98c5d802879abf82")
 
 hunter_cmake_args(gumbo CMAKE_ARGS BUILD_TESTING=OFF)
 
