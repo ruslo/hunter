e180129f83d6b203b33eed30d3a30a55d6ced443
David Hirvonen <dhirvonen@elucideye.com>
2017-10-31 07:34:30 -0400

    update ogg release
    fix include path

--- cmake/projects/ogg/hunter.cmake
+++ cmake/projects/ogg/hunter.cmake
@@ -14,11 +14,11 @@ hunter_add_version(
     PACKAGE_NAME
     ogg
     VERSION
-    1.3.2-p1
+    1.3.2-p2
     URL
-    "https://github.com/hunter-packages/ogg/archive/v1.3.2-p1.tar.gz"
+    "https://github.com/hunter-packages/ogg/archive/v1.3.2-p2.tar.gz"
     SHA1
-    4f3513db3c8a6a616231156225b348deb68ed30e
+    40f04fb995d33fa3d24b0bf3c305b7f2e9a6a33c
     )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
