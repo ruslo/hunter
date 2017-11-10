3a5b2d0b3f896af7ff8f4256d5642041ca1accba
David Hirvonen <dhirvonen@elucideye.com>
2017-11-09 22:23:18 -0500

    WIN32 unistd.h patch

--- cmake/projects/giflib/hunter.cmake
+++ cmake/projects/giflib/hunter.cmake
@@ -14,11 +14,11 @@ hunter_add_version(
     PACKAGE_NAME
     giflib
     VERSION
-    5.1.4
+    5.1.4-p0
     URL
-    "https://github.com/hunter-packages/giflib/archive/v5.1.4.tar.gz"
+    "https://github.com/hunter-packages/giflib/archive/v5.1.4-p0.tar.gz"
     SHA1
-    45f4b0f615b8cc5034a3adcf46f315caccb32988
+    d93b71a805de3c24316e55164828fe687527a8f4
     )
   
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
