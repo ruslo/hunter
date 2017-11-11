81860093a4eafd684dcf615404826ec8a086a963
Yassine MADDOURI <m.y.maddouri@gmail.com>
2017-11-11 14:45:01 +0100

    google/benchmark Clean up hunter.cmake

--- cmake/projects/benchmark/hunter.cmake
+++ cmake/projects/benchmark/hunter.cmake
@@ -6,7 +6,6 @@ include(hunter_cmake_args)
 include(hunter_download)
 include(hunter_pick_scheme)
 
-
 hunter_add_version(
     PACKAGE_NAME benchmark
     VERSION "1.3.0"
@@ -18,7 +17,6 @@ hunter_add_version(
     VERSION "1.2.0"
     URL "https://github.com/google/benchmark/archive/v1.2.0.tar.gz"
     SHA1 "5f26619848c59ddc354e2d51e2f196d2a6ddb189")
-
 
 hunter_cmake_args(benchmark CMAKE_ARGS BENCHMARK_ENABLE_TESTING=OFF)
 
