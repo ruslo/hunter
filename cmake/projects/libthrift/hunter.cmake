932c9837157e437937745856f853674a5d728762
Isaac Hier <isaachier@gmail.com>
2017-11-15 08:59:34 -0500

    Update thrift versions

--- cmake/projects/libthrift/hunter.cmake
+++ cmake/projects/libthrift/hunter.cmake
@@ -9,14 +9,14 @@ include(hunter_pick_scheme)
 hunter_add_version(
     PACKAGE_NAME libthrift
     VERSION "0.10.0"
-    URL "https://github.com/isaachier/thrift/archive/hunter-0.10.0-p6.tar.gz"
-    SHA1 "a5b7ec6920462890641c20e8e289c6a6e9ec206d")
+    URL "https://github.com/isaachier/thrift/archive/hunter-0.10.0-p7.tar.gz"
+    SHA1 "f5cc2fae935fba47ae4b441e0f651bff84a3cc83")
 
 hunter_add_version(
     PACKAGE_NAME libthrift
     VERSION "0.9.2"
-    URL "https://github.com/isaachier/thrift/archive/hunter-0.9.2-p3.tar.gz"
-    SHA1 "c40af1a45072110a7c416fd3b6a2f3326de9b3dc")
+    URL "https://github.com/isaachier/thrift/archive/hunter-0.9.2-p4.tar.gz"
+    SHA1 "9d5b60c9af6583a3d4f516f70db1262de7db1c73")
 
 hunter_cmake_args(libthrift CMAKE_ARGS
     BUILD_TESTING=OFF
