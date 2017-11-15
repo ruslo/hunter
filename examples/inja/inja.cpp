cfef38cfc5e0bafb668dacdab05bce5cebda55cd
Jorrit Wronski <jowr@ipu.dk>
2017-11-15 14:19:34 +0100

    Editing the example

--- examples/inja/inja.cpp
+++ examples/inja/inja.cpp
@@ -1,13 +1,10 @@
 #include <nlohmann/json.hpp>
 #include <inja.hpp>
 
-using json = nlohmann::json;
-using Type = inja::Parser::Type;
-
 int main() {
     inja::Parser parser = inja::Parser();
     std::string test = "lorem ipsum";
-    json result = {{{"type", Type::String}, {"text", "lorem ipsum"}}};
+    nlohmann::json result = {{{"type", inja::Parser::Type::String}, {"text", "lorem ipsum"}}};
     bool test = parser.parse(test) == result;
     return 0;
 }
