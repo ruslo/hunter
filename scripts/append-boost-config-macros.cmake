6eb4ed822c615239db27386689da01c66ea04a87
Isra Basrt0 <ibasrt0@gmail.com>
2017-11-21 14:22:41 +0100

    skip the first three args in append-boost-config-macros.cmake script

--- scripts/append-boost-config-macros.cmake
+++ scripts/append-boost-config-macros.cmake
@@ -6,7 +6,7 @@ set(boost_user_config_file "boost/config/user.hpp")
 set(define_without_value_mode FALSE)
 math(EXPR ARGC_COUNT ${CMAKE_ARGC}-1)
 
-foreach(i RANGE ${ARGC_COUNT})
+foreach(i RANGE 3 ${ARGC_COUNT})
 
   if (define_without_value_mode)
     if ("${CMAKE_ARGV${i}}" MATCHES "[^=]+=.+")
