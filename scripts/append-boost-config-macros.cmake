e4ab64b863b6529f88eb1b4f88897d58d249cf62
Isra Basrt0 <ibasrt0@gmail.com>
2017-11-21 15:39:37 +0100

    don’t use CMAKE_ARGS for the append-boost-config-macros.cmake script
    Use a -DBOOST_CONFIG_MACROS instead.
    
    Parsing of custom args is move to a new helper
    function hunter_parse_boost_config_macros()

--- scripts/append-boost-config-macros.cmake
+++ scripts/append-boost-config-macros.cmake
@@ -3,34 +3,15 @@ cmake_minimum_required(VERSION 3.0)
 # run at the boost source root
 set(boost_user_config_file "boost/config/user.hpp")
 
-set(define_without_value_mode FALSE)
-math(EXPR ARGC_COUNT ${CMAKE_ARGC}-1)
-
-foreach(i RANGE 3 ${ARGC_COUNT})
-
-  if (define_without_value_mode)
-    if ("${CMAKE_ARGV${i}}" MATCHES "[^=]+=.+")
-      set(define_without_value_mode FALSE)
-    endif()
-  endif()
-
+foreach(s ${BOOST_CONFIG_MACROS})
   unset(append_str)
-
-  if(define_without_value_mode)
-      # CONFIG_MACRO=id_1 id_2 ... id_n -> #define id_n
-      set(append_str "#define ${CMAKE_ARGV${i}}")
-  elseif("${CMAKE_ARGV${i}}" MATCHES "^CONFIG_MACRO=(.+)" )
-    # CONFIG_MACRO=id_1 id_2 ... id_n -> #define id_1
-    set(append_str "#define ${CMAKE_MATCH_1}")
-    set(define_without_value_mode TRUE)
-  elseif("${CMAKE_ARGV${i}}" MATCHES "^CONFIG_MACRO_([^=]+)=(.+)" )
-    # CONFIG_MACRO_id=val -> #define id val
+  if("${s}" MATCHES "^([^=]+)=(.+)" )
     set(append_str "#define ${CMAKE_MATCH_1} ${CMAKE_MATCH_2}")
+  else()
+    set(append_str "#define ${s}")
   endif()
-
   if(append_str)
     message("-- append '${append_str}' to '${boost_user_config_file}'")
     file(APPEND "${boost_user_config_file}" "\n${append_str}\n")
   endif()
-
 endforeach()
