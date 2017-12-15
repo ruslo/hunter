9bc9b02f75f70f47e230aeb4d25b9d7d6b2bed94
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-12-15 17:04:22 +0300

    Sort

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -24,6 +24,7 @@ include(hunter_user_error)
 
 # NOTE: no names with spaces!
 
+hunter_config(ARM_NEON_2_x86_SSE VERSION 1.0.0-p0)
 hunter_config(AllTheFlopsThreads VERSION 0.1-p0)
 hunter_config(Android-Apk VERSION 1.1.13)
 hunter_config(Android-Build-Tools VERSION 22.0.1)
@@ -33,7 +34,6 @@ hunter_config(Android-SDK VERSION 0.0.5)
 hunter_config(Android-SDK-Platform-tools VERSION r25.0.5)
 hunter_config(Android-SDK-Tools VERSION 25.2.5)
 hunter_config(Android-Support-Repository VERSION 47)
-hunter_config(ARM_NEON_2_x86_SSE VERSION 1.0.0-p0)
 hunter_config(ArrayFire VERSION 3.3.1-p0)
 hunter_config(Assimp VERSION 3.2-p1)
 hunter_config(Async++ VERSION 0.0.3-hunter)
