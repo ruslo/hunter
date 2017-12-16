f951dab8cb73e6be195d46fccd6d12472ea4bd63
MaximSurovtsev <gr4m0zeka@gmail.com>
2017-12-16 21:32:40 +0300

    treap

---
+++ cmake/projects/Treap/hunter.cmake
@@ -0,0 +1,28 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    Treap
+    VERSION
+    "0.1"
+    URL
+    "https://github.com/BinaryTreesImplementation/Treap/archive/v0.1.tar.gz"
+    SHA1
+    e2a05501b537f45b05ee54bb4e8635c1a4979452
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    hashtable
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(Treap)
+hunter_download(PACKAGE_NAME Treap)
+
