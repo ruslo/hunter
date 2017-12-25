671f1fab9779f9713e7d34b7148a11647c3e7419
Isaac Hier <isaachier@gmail.com>
2017-12-25 15:53:50 +0100

    Follow pattern used in gauze

--- cmake/projects/autoutils/hunter.cmake
+++ cmake/projects/autoutils/hunter.cmake
@@ -1,5 +1,5 @@
 include(hunter_add_version)
-include(hunter_cmake_args)
+include(hunter_cacheable)
 include(hunter_download)
 include(hunter_pick_scheme)
 
@@ -7,11 +7,8 @@ hunter_add_version(
   PACKAGE_NAME autoutils
   VERSION "0.1.0"
   URL "https://github.com/isaachier/autoutils/archive/v0.1.0.tar.gz"
-  SHA1 "defa23744e5fc3b561992f58a1ca5518c9164b3b")
+  SHA1 "5e7a54fe584a6055e54fd4e6fcfc6de1e458eb07")
 
-# Usually there is no need to specify 'LICENSE' explicitly (one of the defaults).
-# Here it's used to update Hunter cache.
-hunter_cmake_args(autoutils CMAKE_ARGS "HUNTER_INSTALL_LICENSE_FILES=LICENSE")
-
-hunter_pick_scheme(DEFAULT url_sha1_unpack)
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(autoutils)
 hunter_download(PACKAGE_NAME autoutils)
