6cd2ab3cc3c2763b354c5141cdad64af6579d00a
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-12-31 01:34:21 +0300

    Android-Apk 1.1.14

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -26,7 +26,7 @@ include(hunter_user_error)
 
 hunter_config(ARM_NEON_2_x86_SSE VERSION 1.0.0-p0)
 hunter_config(AllTheFlopsThreads VERSION 0.1-p0)
-hunter_config(Android-Apk VERSION 1.1.13)
+hunter_config(Android-Apk VERSION 1.1.14)
 hunter_config(Android-Build-Tools VERSION 22.0.1)
 hunter_config(Android-Google-Repository VERSION 47)
 hunter_config(Android-Modules VERSION 1.0.0)
