bbb92d6e2cd2499d9abfe402a2635cc4fa41f0bc
Sacha Refshauge <xsacha@gmail.com>
2018-01-20 19:26:42 +1000

    Add minizip v1.0.1-p3. Update example.

--- cmake/projects/minizip/hunter.cmake
+++ cmake/projects/minizip/hunter.cmake
@@ -12,6 +12,17 @@ hunter_add_version(
     PACKAGE_NAME
     minizip
     VERSION
+    "1.0.1-p3"
+    URL
+    "https://github.com/hunter-packages/minizip/archive/v1.0.1-p3.tar.gz"
+    SHA1
+    9ad9dab6758d7fdbeae492515b7027ecd4903b16
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    minizip
+    VERSION
     "1.0.1-p1"
     URL
     "https://github.com/hunter-packages/minizip/archive/v1.0.1-p1.tar.gz"
--- examples/minizip/foo.cpp
+++ examples/minizip/foo.cpp
@@ -2,7 +2,6 @@
 #include "minizip/aes/aes.h"
 #include "minizip/aes/fileenc.h"
 #include "minizip/aes/prng.h"
-#include "minizip/aes/entropy.h"
 
 int main() {
 }
