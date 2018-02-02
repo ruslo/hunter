be18bcb9493021feaed2f21ffc9de45410c0c0c1
Paweł Bylica <chfast@gmail.com>
2018-02-02 15:19:21 +0100

    Add libscrypt

---
+++ cmake/projects/libscrypt/hunter.cmake
@@ -0,0 +1,23 @@
+# Copyright (c) 2017, Pawel Bylica
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    libscrypt
+    VERSION
+    1.21-p0
+    URL
+    "https://github.com/hunter-packages/libscrypt/archive/v1.21-p0.tar.gz"
+    SHA1
+    2910558f6275fe4ce077038fcb96f7e0ba9521a3
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(libscrypt)
+hunter_download(PACKAGE_NAME libscrypt)
