6580254bdbe4a5742fc27a10110a933c43c6ee38
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-02-05 23:47:53 +0300

    New 'hunter_get_passwords_path' module

--- cmake/modules/hunter_read_http_credentials.cmake
+++ cmake/modules/hunter_read_http_credentials.cmake
@@ -2,6 +2,7 @@
 # All rights reserved.
 
 include(hunter_http_password) # for included module
+include(hunter_get_passwords_path)
 include(hunter_status_debug)
 include(hunter_user_error)
 
@@ -14,36 +15,7 @@ function(hunter_read_http_credentials)
       "Reading HTTP credentials for protected package '${HUNTER_PACKAGE_NAME}'"
   )
 
-  string(COMPARE EQUAL "${HUNTER_PASSWORDS_PATH}" "" cmake_is_empty)
-  string(COMPARE EQUAL "$ENV{HUNTER_PASSWORDS_PATH}" "" env_is_empty)
-  string(COMPARE EQUAL "$ENV{HOME}" "" home_is_empty)
-  string(COMPARE EQUAL "$ENV{USERPROFILE}" "" userprofile_is_empty)
-
-  if(NOT cmake_is_empty)
-    hunter_status_debug("Using CMake variable HUNTER_PASSWORDS_PATH")
-    set(pass_path "${HUNTER_PASSWORDS_PATH}")
-  elseif(NOT env_is_empty)
-    hunter_status_debug("Using environment variable HUNTER_PASSWORDS_PATH")
-    set(pass_path "$ENV{HUNTER_PASSWORDS_PATH}")
-  elseif(home_is_empty AND CMAKE_HOST_WIN32)
-    if(userprofile_is_empty)
-      hunter_user_error(
-          "USERPROFILE environment variable is empty on Windows host"
-      )
-    endif()
-    hunter_status_debug("Using environment variable USERPROFILE")
-    set(pass_path "$ENV{USERPROFILE}/.config/Hunter/passwords.cmake")
-  else()
-    if(home_is_empty)
-      hunter_user_error("HOME environment variable is empty")
-    endif()
-    hunter_status_debug("Using environment variable HOME")
-    set(pass_path "$ENV{HOME}/.config/Hunter/passwords.cmake")
-  endif()
-
-  if(NOT EXISTS "${pass_path}")
-    hunter_user_error("File with passwords not found: '${pass_path}'")
-  endif()
+  hunter_get_passwords_path(pass_path)
 
   hunter_status_debug("Including file '${pass_path}'")
   include("${pass_path}")
