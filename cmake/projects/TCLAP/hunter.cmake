e5679bca5a188a41bd3daa98d2c9c463d686b8eb
Alex Frappier Lachapelle <narutoxela@gmail.com>
2018-02-22 12:37:48 -0500

    Added hunter_config

--- cmake/projects/TCLAP/hunter.cmake
+++ cmake/projects/TCLAP/hunter.cmake
@@ -11,6 +11,17 @@ include(hunter_download)
 include(hunter_pick_scheme)
 
 
+#hunter_add_version(
+#    PACKAGE_NAME
+#    TCLAP
+#    VERSION
+#    "1.2.2-p0"
+#    URL
+#    "https://github.com/hunter-packages/tclap/archive/v1.2.2-p0.zip"
+#    SHA1
+#    5cbc964840ed3d8d2e64bb57fd0c2897ca05e3b0
+#)
+
 hunter_add_version(
     PACKAGE_NAME
     TCLAP
@@ -21,3 +32,8 @@ hunter_add_version(
     SHA1
     5cbc964840ed3d8d2e64bb57fd0c2897ca05e3b0
 )
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(TCLAP)
+hunter_download(PACKAGE_NAME TCLAP)
+
