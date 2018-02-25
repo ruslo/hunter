447712af1a51cb1fba4d0f955419563ea0cfe23e
Alex Frappier Lachapelle <narutoxela@gmail.com>
2018-02-24 20:14:42 -0500

    Fixed hunter build and added basic example

--- cmake/projects/TCLAP/hunter.cmake
+++ cmake/projects/TCLAP/hunter.cmake
@@ -26,11 +26,11 @@ hunter_add_version(
     PACKAGE_NAME
     TCLAP
     VERSION
-    1.2.2-p0
+    1.2.2-p1
     URL
-    "https://github.com/hunter-packages/tclap/archive/v1.2.2-p0.zip"
+    "https://github.com/hunter-packages/tclap/archive/v1.2.2-p1.zip"
     SHA1
-    5cbc964840ed3d8d2e64bb57fd0c2897ca05e3b0
+    c31f3b37c5f1cc34e802000e338d3f04a4d5da79
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
