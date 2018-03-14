6aba284dbbfe4217a793fe503d8e183d8d94078b
Isaac Hier <isaachier@gmail.com>
2018-03-14 09:42:32 -0400

    Fix example

--- examples/pcg/main.c
+++ examples/pcg/main.c
@@ -3,7 +3,8 @@
 
 int main()
 {
+    pcg32_random_t state;
     uint64_t seed[2];
-    pcg32_srandom_r(&rng->state, seed[0], seed[1]);
+    pcg32_srandom_r(&state, seed[0], seed[1]);
     return 0;
 }
