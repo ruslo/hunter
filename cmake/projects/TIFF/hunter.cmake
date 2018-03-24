a8706bd4167e2e37a46524962909e6e107745996
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-03-24 21:46:04 +0300

    TIFF 4.0.2-p4

--- cmake/projects/TIFF/hunter.cmake
+++ cmake/projects/TIFF/hunter.cmake
@@ -52,6 +52,17 @@ hunter_add_version(
     37c71656488797c4e5fde620570f1a1b9be36037
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    TIFF
+    VERSION
+    "4.0.2-p4"
+    URL
+    "https://github.com/hunter-packages/tiff/archive/v4.0.2-p4.tar.gz"
+    SHA1
+    9ab44e0136d3da04b0a559d7ba57bd075afc48f9
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(TIFF)
 hunter_download(PACKAGE_NAME TIFF)
