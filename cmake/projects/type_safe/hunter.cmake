0d98afd7c00ada2dfee6031a80446223c269606c
dvirtz <dvirtz@gmail.com>
2018-04-02 16:54:44 +0300

    type_safe 0.1-p2

--- cmake/projects/type_safe/hunter.cmake
+++ cmake/projects/type_safe/hunter.cmake
@@ -20,6 +20,17 @@ hunter_add_version(
     57e426af70791485e2a209c602ce9d71e9d52ab7
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    type_safe
+    VERSION
+    "0.1-p2"
+    URL
+    "https://github.com/hunter-packages/type_safe/archive/0.1-p2.tar.gz"
+    SHA1
+    6a1fa6f3926a5d0175890647ff0f419ba4c562f4
+)
+
 # Pick a download scheme
 hunter_pick_scheme(DEFAULT url_sha1_cmake) # use scheme for cmake projects
 hunter_cacheable(type_safe)
