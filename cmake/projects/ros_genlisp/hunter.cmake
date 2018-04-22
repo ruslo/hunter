f53681c1bf9fdf54b8233a3ba594ae9f15251109
Lukas Solanka <lsolanka@gmail.com>
2018-04-22 22:08:30 +0100

    add ros-genlisp 0.4.16-p0

--- cmake/projects/ros_genlisp/hunter.cmake
+++ cmake/projects/ros_genlisp/hunter.cmake
@@ -11,21 +11,21 @@ include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME
-    ros_geneus
+    ros_genlisp
     VERSION
-    2.2.6-p0
+    0.4.16-p0
     URL
-    "https://github.com/hunter-packages/ros_geneus/archive/v2.2.6-p0.tar.gz"
+    "https://github.com/hunter-packages/ros_genlisp/archive/v0.4.16-p0.tar.gz"
     SHA1
-    "938a53b42aefca1a1d80384bc5f43ba83e4eed88"
+    "5077d8b5cf48f4224fc13659eccd5d7b89062609"
 )
 
 hunter_cmake_args(
-    ros_geneus
+    ros_genlisp
     CMAKE_ARGS
     CATKIN_ENABLE_TESTING:BOOL=OFF
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
-hunter_cacheable(ros_geneus)
-hunter_download(PACKAGE_NAME ros_geneus)
+hunter_cacheable(ros_genlisp)
+hunter_download(PACKAGE_NAME ros_genlisp)
--- docs/packages/robotics.rst
+++ docs/packages/robotics.rst
@@ -3,6 +3,7 @@
     catkin
     gencpp
     geneus
+    genlisp
     genmsg
     ros_genmsg
     ros
@@ -17,5 +18,6 @@ ROS
  * :ref:`pkg.ros_console_bridge` - ROS console bridge package (logging, ...)
  * :ref:`pkg.ros_gencpp` - ROS gencpp package - C++ message and service data structure generation
  * :ref:`pkg.ros_geneus` - ROS geneus package - EusLisp ROS message and service generators
+ * :ref:`pkg.ros_genlisp` - ROS genlisp package - Lisp message generation for ROS
  * :ref:`pkg.ros_genmsg` - ROS genmsg package - message and service data structure generation
  * :ref:`pkg.roscpp_core` - ROS C++ core package
