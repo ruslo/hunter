70441b6a9c585b8a6ddb04f18e61469c62ec7efc
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-04-23 21:09:47 +0300

    WebKit 0.0.2-p0

--- cmake/projects/WebKit/hunter.cmake
+++ cmake/projects/WebKit/hunter.cmake
@@ -21,6 +21,17 @@ hunter_add_version(
     2da25c0485984e8ffb236050316d454e6c0ff849
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    WebKit
+    VERSION
+    0.0.2-p0
+    URL
+    "https://github.com/hunter-packages/WebKit/archive/v0.0.2-p0.tar.gz"
+    SHA1
+    a7becb00409bd2731040b175dd4fe69792014714
+)
+
 hunter_cmake_args(
     WebKit
     CMAKE_ARGS
