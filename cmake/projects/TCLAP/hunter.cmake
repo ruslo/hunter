2607bb7fbc2f64134ffabe00f62e1085ced38f94
Alex Frappier Lachapelle <narutoxela@gmail.com>
2018-05-01 05:37:27 -0400

    Removed broken version.

--- cmake/projects/TCLAP/hunter.cmake
+++ cmake/projects/TCLAP/hunter.cmake
@@ -10,18 +10,6 @@ include(hunter_cacheable)
 include(hunter_download)
 include(hunter_pick_scheme)
 
-
-#hunter_add_version(
-#    PACKAGE_NAME
-#    TCLAP
-#    VERSION
-#    "1.2.2-p0"
-#    URL
-#    "https://github.com/hunter-packages/tclap/archive/v1.2.2-p0.zip"
-#    SHA1
-#    5cbc964840ed3d8d2e64bb57fd0c2897ca05e3b0
-#)
-
 hunter_add_version(
     PACKAGE_NAME
     TCLAP
