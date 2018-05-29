2be4f79ccb01119d134bc21fe1d8bdde2d3ffd7d
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-05-30 01:27:45 +0300

    OpenSSL: Remove leading whitespaces

--- cmake/projects/OpenSSL/hunter.cmake
+++ cmake/projects/OpenSSL/hunter.cmake
@@ -377,4 +377,4 @@ if(MINGW)
 endif()
 
 hunter_cacheable(OpenSSL)
-hunter_download(PACKAGE_NAME OpenSSL PACKAGE_INTERNAL_DEPS_ID "20")
+hunter_download(PACKAGE_NAME OpenSSL PACKAGE_INTERNAL_DEPS_ID "21")
