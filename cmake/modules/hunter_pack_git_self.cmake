339e4047f7fea42aee5d838fad0127c363e1af0d
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-06-11 11:16:20 +0300

    Add log message

--- cmake/modules/hunter_pack_git_self.cmake
+++ cmake/modules/hunter_pack_git_self.cmake
@@ -54,6 +54,10 @@ function(hunter_pack_git_self)
   endif()
 
   set(cmd "${git_executable}" status --porcelain)
+  hunter_status_debug(
+      "Run command '${cmd}' in '${top_git_directory}' directory"
+  )
+  hunter_status_debug("HUNTER_GATE_LOCATION: ${HUNTER_GATE_LOCATION}")
   execute_process(
       COMMAND ${cmd}
       WORKING_DIRECTORY "${top_git_directory}"
