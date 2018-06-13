5970826e923cf961afb7bd578ded840b0a72eb75
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-06-13 12:24:56 +0300

    OpenSSL: ++PACKAGE_INTERNAL_DEPS_ID

--- cmake/projects/OpenSSL/hunter.cmake
+++ cmake/projects/OpenSSL/hunter.cmake
@@ -388,4 +388,4 @@ if(MINGW)
 endif()
 
 hunter_cacheable(OpenSSL)
-hunter_download(PACKAGE_NAME OpenSSL PACKAGE_INTERNAL_DEPS_ID "22")
+hunter_download(PACKAGE_NAME OpenSSL PACKAGE_INTERNAL_DEPS_ID "23")
