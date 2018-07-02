109329ac413ba38ecdb9a3bf7e712a0a4bc2df3d
Dinesh Manajipet <saidinesh5@gmail.com>
2018-07-03 03:01:12 +0530

    Update libsodium to 1.0.16

--- cmake/projects/libsodium/hunter.cmake
+++ cmake/projects/libsodium/hunter.cmake
@@ -18,6 +18,18 @@ hunter_add_version(
     00a999629f1f8f500cef29818135f8f889f44e41
 )
 
+
+hunter_add_version(
+    PACKAGE_NAME
+    libsodium
+    VERSION
+    "1.0.16"
+    URL
+    "https://github.com/jedisct1/libsodium/archive/1.0.16.tar.gz"
+    SHA1
+    18a2c5758aaf60c0abbc0e6a21f63df80faccfc6
+)
+
 hunter_configuration_types(libsodium CONFIGURATION_TYPES Release)
 if(MSVC)
     hunter_pick_scheme(DEFAULT url_sha1_libsodium_msbuild)
