444456926045aadb0d6a1d60cb1afa0bec5fc698
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-07-05 02:12:30 +0300

    Add freetype 2.4.12-p0

--- cmake/projects/freetype/hunter.cmake
+++ cmake/projects/freetype/hunter.cmake
@@ -18,6 +18,17 @@ hunter_add_version(
     d797e30cfa79eb95dc3b67e75fa6d6a3da80d1ae
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    freetype
+    VERSION
+    "2.4.12-p0"
+    URL
+    "https://github.com/hunter-packages/freetype/archive/v2.4.12-p0.tar.gz"
+    SHA1
+    6623f3b91b921c71c4b4485f67b252a7025eb18e
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(freetype)
 hunter_download(PACKAGE_NAME freetype)
