81dba1109a21b4d2b7bc38b97d491481f342f33f
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-07-27 22:25:32 +0200

    Bump CLI11 version to 1.6.1, remove single file generation

--- cmake/projects/CLI11/hunter.cmake
+++ cmake/projects/CLI11/hunter.cmake
@@ -10,14 +10,48 @@ include(hunter_download)
 include(hunter_pick_scheme)
 
 hunter_add_version(
+     PACKAGE_NAME
+     CLI11
+     VERSION
+     1.5.3
+     URL
+     "https://github.com/CLIUtils/CLI11/archive/v1.5.3.tar.gz"
+     SHA1
+     00c982a7e6819524acda111515ea02308e93a92f
+ )
+
+hunter_add_version(
+     PACKAGE_NAME
+     CLI11
+     VERSION
+     1.5.4
+     URL
+     "https://github.com/CLIUtils/CLI11/archive/v1.5.4.tar.gz"
+     SHA1
+     94c6860aaa13b16c47e939194a2cdd26e869256c
+ )
+
+hunter_add_version(
+     PACKAGE_NAME
+     CLI11
+     VERSION
+     1.6.0
+     URL
+     "https://github.com/CLIUtils/CLI11/archive/v1.6.0.tar.gz"
+     SHA1
+     f49b36ca4707dc89cb2bd03fc4f7558e30417afc
+ )
+
+
+hunter_add_version(
     PACKAGE_NAME
     CLI11
     VERSION
-    1.5.3
+    1.6.1
     URL
-    "https://github.com/CLIUtils/CLI11/archive/v1.5.3.tar.gz"
+    "https://github.com/CLIUtils/CLI11/archive/v1.6.1.tar.gz"
     SHA1
-    00c982a7e6819524acda111515ea02308e93a92f
+    4d900c543cc73b80bc31da20998b2d516d67e0ae
 )
 
 hunter_cmake_args(
@@ -25,6 +59,7 @@ hunter_cmake_args(
     CMAKE_ARGS
     CLI11_TESTING=OFF
     CLI11_EXAMPLES=OFF
+    CLI11_SINGLE_FILE=OFF
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
