efa4bd745403f6d502bd34db5ccf96edcc5f073e
t0p4 <t0p4skill@gmail.com>
2018-10-09 14:37:52 +0300

    Update hunter.cmake

--- cmake/projects/tommath/hunter.cmake
+++ cmake/projects/tommath/hunter.cmake
@@ -25,9 +25,9 @@ hunter_add_version(
     VERSION
     "1.0.1"
     URL
-    "https://github.com/t0p4/libtommath/archive/v1.0.1-p1.tar.gz"
+    "https://github.com/hunter-packages/libtommath/archive/v1.0.1-p0.tar.gz"
     SHA1
-    0A581D4DBFC0229712E21A849FE5612C301589F0
+    6270FF3C125D0C513AEB10F62EF25D4EB341AF37
 )
 # Pick a download scheme
 hunter_pick_scheme(DEFAULT url_sha1_cmake) # use scheme for cmake projects
