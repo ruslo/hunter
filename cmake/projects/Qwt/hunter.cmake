a62ca2580f7b4d445095154074f9dd11c6185067
t0p4 <>
2018-11-21 16:16:38 +0200

    Fix url

--- cmake/projects/Qwt/hunter.cmake
+++ cmake/projects/Qwt/hunter.cmake
@@ -15,7 +15,7 @@ hunter_add_version(
     VERSION
     6.1-p3
     URL
-    https://github.com/t0p4/Qwt/archive/6.1-p3tar.gz
+    https://github.com/t0p4/Qwt/archive/6.1-p3.tar.gz
     SHA1
     9db2e7c254c181b43de414b05cca38152e4b6f88
 )
