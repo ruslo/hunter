86802f9f990ddf4ac019127ab720af79dab30772
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-11-21 23:09:59 +0300

    LLVM 6.0.1-p0

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -78,7 +78,7 @@ hunter_default_version(Igloo VERSION 1.1.1-hunter)
 hunter_default_version(Jpeg VERSION 9b-p3)
 hunter_default_version(JsonSpirit VERSION 0.0.4-hunter)
 hunter_default_version(LAPACK VERSION 3.7.1)
-hunter_default_version(LLVM VERSION 4.0.1-p0) # Clang
+hunter_default_version(LLVM VERSION 6.0.1-p0) # Clang
 hunter_default_version(LLVMCompilerRT VERSION 6.0.1) # Clang
 hunter_default_version(Leathers VERSION 0.1.8)
 hunter_default_version(Leptonica VERSION 1.74.2-p4)
