a5c2402a89d8275eb4b2fa6404d72f5c68425bfc
craffael <raffael@casagrande.ch>
2018-11-29 05:52:17 +0100

    Add `lehrfempp` package (#1629)

---
+++ cmake/projects/lehrfempp/hunter.cmake
@@ -0,0 +1,32 @@
+# Copyright (c) 2016-2018, Ruslan Baratov
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    lehrfempp
+    VERSION
+    0.7.3
+    URL
+    "https://github.com/craffael/lehrfempp/archive/release-0.7.3.tar.gz"
+    SHA1
+    228512a28abe9e49484d1794a20657a47fbf5a1d
+)
+
+hunter_cmake_args(
+    lehrfempp
+    CMAKE_ARGS
+        LF_ENABLE_TESTING=OFF
+        LF_BUILD_EXAMPLES=OFF
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(lehrfempp)
+hunter_download(PACKAGE_NAME lehrfempp)
---
+++ docs/packages/pkg/lehrfempp.rst
@@ -0,0 +1,19 @@
+.. spelling::
+    lehrfempp
+    
+.. index::
+  single: math ; lehrfempp
+
+.. _pkg.lehrfempp:
+
+lehrfempp
+=========
+
+-  `Official <https://github.com/craffael/lehrfempp>`__
+-  `Example <https://github.com/ruslo/hunter/blob/master/examples/lehrfempp/CMakeLists.txt>`__
+-  Added by `craffael <https://github.com/craffael>`__ (`pr-1629 <https://github.com/ruslo/hunter/pull/1629>`__)
+
+.. literalinclude:: /../examples/lehrfempp/CMakeLists.txt
+  :language: cmake
+  :start-after: # DOCUMENTATION_START {
+  :end-before: # DOCUMENTATION_END }
