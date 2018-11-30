544a9afdd6dd54bfe6d2fdbbaf770c01fdbd0329
Brad Kotsopoulos <bkotsopoulos@snapchat.com>
2018-11-30 13:09:36 -0500

    Update to new release

--- cmake/projects/LodePNG/hunter.cmake
+++ cmake/projects/LodePNG/hunter.cmake
@@ -12,11 +12,11 @@ hunter_add_version(
     PACKAGE_NAME
     LodePNG
     VERSION
-    "0.0.0-p0"
+    "0.0.0-p1"
     URL
-    "https://github.com/hunter-packages/lodepng/archive/v0.0.0-p0.tar.gz"
+    "https://github.com/hunter-packages/lodepng/archive/v0.0.0-p1.tar.gz"
     SHA1
-    262cd3528f73e917c560ff7d9c0bdd9b4f1cc761
+    315129891c5ea6c922ed98398c853a7a83a3c662
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
