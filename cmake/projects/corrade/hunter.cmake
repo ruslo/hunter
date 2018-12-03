933817fd0ac4990825593221029806de1c2c741a
Pascal Thomet <pthomet@gmail.com>
2018-12-03 19:31:34 +0100

    corrade: use the official v2018.10 release

--- cmake/projects/corrade/hunter.cmake
+++ cmake/projects/corrade/hunter.cmake
@@ -8,24 +8,11 @@ include(hunter_pick_scheme)
 include(hunter_cmake_args)
 
 # List of versions here...
-
-
 hunter_add_version(
     PACKAGE_NAME
     corrade
     VERSION
-    hunter_rc1
-    URL
-    "https://github.com/pthom/corrade/archive/hunter_rc1.tar.gz"
-    SHA1
-    1a6ba9b925eb2c01e6e91d9c7e2c029d4572d57d
-)
-  
-hunter_add_version(
-    PACKAGE_NAME
-    corrade
-    VERSION
-    "v2018.10"
+    v2018.10
     URL
     "https://github.com/mosra/corrade/archive/v2018.10.tar.gz"
     SHA1
@@ -35,32 +22,18 @@ hunter_add_version(
 hunter_cmake_args(
     corrade
     CMAKE_ARGS
-        # //Build TestSuite library (these are not tests, but a test library)
+        # Build TestSuite library (these are not tests, but a test library)
         WITH_TESTSUITE=ON
-        # //Build PluginManager library
+        # Build PluginManager library
         WITH_PLUGINMANAGER=ON
-        # //Build Interconnect library
+        # Build Interconnect library
         WITH_INTERCONNECT=ON
+        # Enable compatibility mode for MSVC 2015 (might disable some features)
+        MSVC2015_COMPATIBILITY=OFF
+        # Enable compatibility mode for GCC 4.7 (might disable some features)
+        GCC47_COMPATIBILITY:BOOL=OFF
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(corrade)
 hunter_download(PACKAGE_NAME corrade)
-
-# Other options:
-# //Prefix where to put platform-independent include and other files
-# CORRADE_INCLUDE_INSTALL_PREFIX:STRING=.
-# //Enable compatibility mode for MSVC 2015 (might disable some features)
-# MSVC2015_COMPATIBILITY:BOOL=OFF
-
-# //Enable compatibility mode for MSVC 2017 (might disable some features)
-# MSVC2017_COMPATIBILITY:BOOL=OFF
-
-# //Build TestSuite tests compatible with Xcode XCTest
-# TESTSUITE_TARGET_XCTEST:BOOL=OFF
-
-# //Enable compatibility mode for GCC 4.7 (might disable some features)
-# GCC47_COMPATIBILITY:BOOL=OFF
-
-# //Library directory suffix (e.g. 64 for /usr/lib64).
-# LIB_SUFFIX:STRING=
