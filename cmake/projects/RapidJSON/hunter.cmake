1303af3832a0708f5a83f95acd2ed1111cd77d94
Brad Kotsopoulos <bkotsopoulos@snap.com>
2018-12-10 00:13:27 -0500

    Use hunter archive, update version

--- cmake/projects/RapidJSON/hunter.cmake
+++ cmake/projects/RapidJSON/hunter.cmake
@@ -13,9 +13,9 @@ hunter_add_version(
     PACKAGE_NAME
     RapidJSON
     VERSION
-    "1.1.0-p1"
+    "1.1.0-66eb606-p0"
     URL
-    "https://github.com/bkotzz/rapidjson/archive/v1.1.0-66eb606-p1.tar.gz"
+    "https://github.com/hunter-packages/rapidjson/archive/v1.1.0-66eb606-p0.tar.gz"
     SHA1
     947c0196612c9be6c39c496f84b03234495af5b1
 )
