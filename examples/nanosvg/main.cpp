34647d0e1fb2e9a7ec4b9ef7363c1a553ba940aa
Brad Kotsopoulos <brad.kotsopoulos@gmail.com>
2018-12-10 17:46:31 -0500

    Add nanosvg package (#1658)

---
+++ examples/nanosvg/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+#define NANOSVG_IMPLEMENTATION
+#include <nanosvg/nanosvg.h>
+
+// See https://github.com/memononen/nanosvg/tree/master/example
+// for further examples.
+int main() {
+	NSVGimage image;
+	(void)image;
+}
