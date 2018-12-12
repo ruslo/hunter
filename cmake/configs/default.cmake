83150cdd780e11cf22c310c7b7b1d534b1a9fd2e
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-12-12 13:45:57 +0300

    Boost 1.69.0-p0

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -37,7 +37,7 @@ if(MINGW)
   # https://github.com/boostorg/build/issues/301
   hunter_default_version(Boost VERSION 1.64.0)
 else()
-  hunter_default_version(Boost VERSION 1.68.0-p1)
+  hunter_default_version(Boost VERSION 1.69.0-p0)
 endif()
 
 hunter_default_version(BoostCompute VERSION 0.5-p0)
