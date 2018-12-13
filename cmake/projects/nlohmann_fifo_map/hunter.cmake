db728aca5899607dd6355debf1eb63b1fd282f25
Rahul Sheth <rahul@snap.com>
2018-12-13 15:31:08 -0800

    update version and example

--- cmake/projects/nlohmann_fifo_map/hunter.cmake
+++ cmake/projects/nlohmann_fifo_map/hunter.cmake
@@ -12,11 +12,11 @@ hunter_add_version(
     PACKAGE_NAME
     nlohmann_fifo_map
     VERSION
-    0.0.0-0dfbf5d-p0
+    "0.0.0-0dfbf5d-p1"
     URL
-    "https://github.com/hunter-packages/fifo_map/archive/v0.0.0-0dfbf5d-p0.tar.gz"
+    "https://github.com/hunter-packages/fifo_map/archive/v0.0.0-0dfbf5d-p1.tar.gz"
     SHA1
-    2c72361146d35acb502b2dc394c383a74e6075cf
+    5361545721631631fd0cb4112f888cba8a2ef7ef
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
