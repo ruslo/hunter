381290be0cda1acb2327c3e2e4254feda3d28006
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-12-20 00:46:00 +0300

    gauze 0.6.0

--- cmake/projects/gauze/hunter.cmake
+++ cmake/projects/gauze/hunter.cmake
@@ -119,6 +119,17 @@ hunter_add_version(
     3fff909740e22ce789c57eba7eb35dce916957b3
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    gauze
+    VERSION
+    0.6.0
+    URL
+    "https://github.com/hunter-packages/gauze/archive/v0.6.0.tar.gz"
+    SHA1
+    be82af13be82d92809fb0d132b9d7e554d06c07b
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(gauze)
 hunter_cmake_args(gauze CMAKE_ARGS GAUZE_BUILD_TESTS=OFF)
