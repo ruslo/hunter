c54b2edbaa2b10fd802beaea91dde1cd631aae43
Gregory Kramida <algomorph@gmail.com>
2018-12-21 13:49:42 -0500

    rename PYTHON_VERSION arg, fix Boost jamfile python line
    1) Rename REQUESTED_PYTHON_VERSION --> PYTHON_VERSION for CMAKE_ARGS of the Boost package in
    2) Use <major_version>.<minor_version> for "use python" line in Boost user_config.jam file, instead of <major.minor.patch>, to avert Boost warning

--- examples/Boost-python/config.cmake
+++ examples/Boost-python/config.cmake
@@ -1,3 +1,3 @@
-# Note: REQUESTED_PYTHON_VERSION is optional. Refer to Boost package documentation on how
+# Note: PYTHON_VERSION is optional. Refer to Boost package documentation on how
 # and when to use it: https://docs.hunter.sh/en/latest/packages/pkg/Boost.html#cmake-options
-hunter_config(Boost VERSION ${HUNTER_Boost_VERSION} CMAKE_ARGS REQUESTED_PYTHON_VERSION=${REQUESTED_PYTHON_VERSION})
+hunter_config(Boost VERSION ${HUNTER_Boost_VERSION} CMAKE_ARGS PYTHON_VERSION=${PYTHON_VERSION})
