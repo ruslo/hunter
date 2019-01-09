36aa55067e075af463f1ce9d4a31b1bc8b2cd3a9
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-01-09 14:34:06 +0300

    Update ICU example

--- examples/ICU/boo.cpp
+++ examples/ICU/boo.cpp
@@ -1,4 +1,16 @@
-#include <unicode/utypes.h>
+#include <iostream>
 
-int main() {
+#include <unicode/utypes.h>
+#include <unicode/unistr.h>
+
+int main()
+{
+  const std::string greek_utf8 = "γεια σας";
+  const icu::UnicodeString greek = icu::UnicodeString::fromUTF8(greek_utf8);
+
+  if (greek.countChar32() != 8)
+  {
+    std::cerr << "Sanity check failed" << std::endl;
+    return 1;
+  }
 }
