5f21d6e8dad3f29cf6f756a1a05f1f5b91a3d671
Bogdan Vaneev <warchantua@gmail.com>
2019-02-15 23:20:46 +0200

    Fix build
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- cmake/projects/iroha-ed25519/hunter.cmake
+++ cmake/projects/iroha-ed25519/hunter.cmake
@@ -7,6 +7,7 @@ include(hunter_add_version)
 include(hunter_cacheable)
 include(hunter_download)
 include(hunter_pick_scheme)
+include(hunter_cmake_args)
 
 hunter_add_version(
     PACKAGE_NAME
@@ -17,6 +18,16 @@ hunter_add_version(
     "https://github.com/hyperledger/iroha-ed25519/archive/2.0.0.tar.gz"
     SHA1
     35f683a982da0bf9f291c8ec987dbba7caeafad3
+)
+
+# by default, use portable ref10 implementation with default (from ed25519 spec)
+# sha2 hashing algororithm, provided by sha2_sphlib implementation
+hunter_cmake_args(
+    iroha-ed25519
+    CMAKE_ARGS
+        TESTING=OFF
+        EDIMPL=ref10
+        HASH=sha2_sphlib
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
--- examples/iroha-ed25519/boo.cpp
+++ examples/iroha-ed25519/boo.cpp
@@ -7,12 +7,12 @@ int main(int arc, char** argv) {
   ed25519_create_keypair(&priv, &pub);
 
   const int size      = 7;
-  unsigned char *buff = "ed25519";
+  const char *buff = "ed25519";
 
   signature_t sig;
-  ed25519_sign(&sig, buff, size, &pub, &priv);
+  ed25519_sign(&sig, (unsigned char*)buff, size, &pub, &priv);
 
-  ed25519_verify(&sig, buff, size, &pub);
+  ed25519_verify(&sig, (unsigned char*)buff, size, &pub);
   // return 1 = ok
 
   return 0;
