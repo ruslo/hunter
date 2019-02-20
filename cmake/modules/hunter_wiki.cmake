8e5ce07eb0a9f313bc18ea8bfaacb1517455c8ab
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-02-20 15:50:17 +0300

    Rename variables

--- cmake/modules/hunter_wiki.cmake
+++ cmake/modules/hunter_wiki.cmake
@@ -2,12 +2,12 @@
 # All rights reserved.
 
 # Do not move to master - build schemes use this module but they not use master
-set(HUNTER_WIKI "https://docs.hunter.sh/en/latest/reference/errors")
+set(HUNTER_ERROR_PAGE "https://docs.hunter.sh/en/latest/reference/errors")
 
-function(hunter_wiki wiki_page)
-  message("------------------------------ WIKI -------------------------------")
-  message("    ${HUNTER_WIKI}/${wiki_page}.html")
-  message("-------------------------------------------------------------------")
+function(hunter_wiki error_page)
+  message("------------------------------ ERROR -----------------------------")
+  message("    ${HUNTER_ERROR_PAGE}/${error_page}.html")
+  message("------------------------------------------------------------------")
   message("")
   message(FATAL_ERROR "")
 endfunction()
