346c3dc21492a1066234714f3a46b293e97b3456
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-02-21 16:44:36 +0300

    freetype 2.9.1-p0

--- cmake/projects/freetype/hunter.cmake
+++ cmake/projects/freetype/hunter.cmake
@@ -4,8 +4,20 @@
 
 include(hunter_add_version)
 include(hunter_cacheable)
+include(hunter_cmake_args)
 include(hunter_download)
 include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    freetype
+    VERSION
+    "2.9.1-p0"
+    URL
+    "https://github.com/hunter-packages/freetype/archive/v2.9.1-p0.tar.gz"
+    SHA1
+    e3b929a3af402059e88c847dd9b92e92c10401fe
+)
 
 hunter_add_version(
     PACKAGE_NAME
@@ -38,6 +50,15 @@ hunter_add_version(
     "https://github.com/hunter-packages/freetype/archive/v2.4.12-p1.tar.gz"
     SHA1
     7f172287e594c916150f1b2a2882a188f945e969
+)
+
+hunter_cmake_args(
+    freetype
+    CMAKE_ARGS
+    FT_WITH_PNG=ON
+    FT_WITH_ZLIB=ON
+    FT_WITH_BZIP2=ON
+    FT_WITH_HARFBUZZ=OFF # harfbuzz already depends on freetype!
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
