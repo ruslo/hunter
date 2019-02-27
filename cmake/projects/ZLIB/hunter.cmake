7428d464ebba3f608d53f4efc119bc5800eb5a16
Harry Mallon <hjmallon@gmail.com>
2019-02-27 15:05:40 +0000

    Update 'ZLIB' to v1.2.11-p0

--- cmake/projects/ZLIB/hunter.cmake
+++ cmake/projects/ZLIB/hunter.cmake
@@ -12,22 +12,11 @@ hunter_add_version(
     PACKAGE_NAME
     ZLIB
     VERSION
-    "1.2.8-p3"
+    "1.2.8-hunter"
     URL
-    "https://github.com/hunter-packages/zlib/archive/v1.2.8-p3.tar.gz"
+    "https://github.com/hunter-packages/zlib/archive/v1.2.8-hunter.tar.gz"
     SHA1
-    573dc28474be47d0c7abc1475a14aa12f0dfcadc
-)
-
-hunter_add_version(
-    PACKAGE_NAME
-    ZLIB
-    VERSION
-    "1.2.8-p2"
-    URL
-    "https://github.com/hunter-packages/zlib/archive/v1.2.8-p2.tar.gz"
-    SHA1
-    bef3ee6d68a271dfcd2f659c80d721d4a6b39315
+    75a05fcc928ed52e1eeb93f07a1c78a7890860c0
 )
 
 hunter_add_version(
@@ -45,18 +34,39 @@ hunter_add_version(
     PACKAGE_NAME
     ZLIB
     VERSION
-    "1.2.8-hunter"
+    "1.2.8-p2"
     URL
-    "https://github.com/hunter-packages/zlib/archive/v1.2.8-hunter.tar.gz"
+    "https://github.com/hunter-packages/zlib/archive/v1.2.8-p2.tar.gz"
     SHA1
-    75a05fcc928ed52e1eeb93f07a1c78a7890860c0
+    bef3ee6d68a271dfcd2f659c80d721d4a6b39315
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    ZLIB
+    VERSION
+    "1.2.8-p3"
+    URL
+    "https://github.com/hunter-packages/zlib/archive/v1.2.8-p3.tar.gz"
+    SHA1
+    573dc28474be47d0c7abc1475a14aa12f0dfcadc
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    ZLIB
+    VERSION
+    "1.2.11-p0"
+    URL
+    "https://github.com/hunter-packages/zlib/archive/v1.2.11-p0.tar.gz"
+    SHA1
+    9ced8b310aa341af7f3584bac0b2d2a669d2178b
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(ZLIB)
-hunter_download(PACKAGE_NAME ZLIB 
+hunter_download(PACKAGE_NAME ZLIB
     PACKAGE_INTERNAL_DEPS_ID "1"
-    PACKAGE_UNRELOCATABLE_TEXT_FILES 
+    PACKAGE_UNRELOCATABLE_TEXT_FILES
     "share/pkgconfig/zlib.pc"
 )
-
