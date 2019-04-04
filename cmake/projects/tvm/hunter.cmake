a4c8d11bdddbd7c9751239bda1c3284c66ac8219
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-04-04 21:21:03 +0300

    tvm 0.5-p4

--- cmake/projects/tvm/hunter.cmake
+++ cmake/projects/tvm/hunter.cmake
@@ -64,6 +64,17 @@ hunter_add_version(
     ddc16b4125b7eafbe34b63da50e7622fe961787e
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    tvm
+    VERSION
+    0.5-p4
+    URL
+    "https://github.com/hunter-packages/tvm/archive/v0.5-p4.tar.gz"
+    SHA1
+    f62ace1e9515799de83b29c6325588e48fc76479
+)
+
 set(
     __hunter_tvm_cmake_args
     INSTALL_DEV=ON
