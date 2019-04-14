e251cc2cc582c4ebb3839194871e10512f344364
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-04-14 20:12:03 +0300

    pip_urllib3 1.24.1

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -373,6 +373,7 @@ hunter_default_version(pip_numpy VERSION 1.16.2)
 hunter_default_version(pip_pylint VERSION 2.3.1)
 hunter_default_version(pip_six VERSION 1.12.0)
 hunter_default_version(pip_smmap VERSION 2.0.5)
+hunter_default_version(pip_urllib3 VERSION 1.24.1)
 hunter_default_version(pip_wrapt VERSION 1.11.1)
 
 hunter_default_version(poly2tri VERSION 1.0.0)
