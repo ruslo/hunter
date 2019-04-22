6c70b7983d4a1e99e187d9de6417296d4eea8353
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-04-22 11:41:32 +0300

    shaka_player_embedded 0.1.0-beta-p0

---
+++ examples/shaka_player_embedded/config.cmake
@@ -0,0 +1,12 @@
+# DOCUMENTATION_START {
+hunter_config(
+    CURL
+    VERSION ${HUNTER_CURL_VERSION}
+    CMAKE_ARGS CMAKE_USE_BORINGSSL=ON
+)
+
+hunter_config(
+    v8
+    VERSION 3.29.86-90da229-p0
+)
+# DOCUMENTATION_END }
