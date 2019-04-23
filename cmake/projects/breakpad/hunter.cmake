05c1442192f53e20f8da845acd31ce4d504c0f1f
Fredrik Appelros <fredrik.appelros@hypergene.se>
2019-04-23 14:11:22 +0200

    Update 'breakpad' to 0.0.0-12ecff3-p2

--- cmake/projects/breakpad/hunter.cmake
+++ cmake/projects/breakpad/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     6d6ba5d3f0b8d317b61d1a4256dcdd55f29d19ee
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    breakpad
+    VERSION
+    0.0.0-12ecff3-p2
+    URL
+    "https://github.com/hunter-packages/breakpad/archive/v0.0.0-12ecff3-p2.tar.gz"
+    SHA1
+    583524ee8f08d0b036bfb4ad3566144b4c272965
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(breakpad)
 hunter_download(PACKAGE_NAME breakpad)
