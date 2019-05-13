77fdbed46c8fdf09f09292a1bf719212ad819b91
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-05-13 16:04:58 +0300

    Update CryptoPP example

--- examples/cryptopp/main.cpp
+++ examples/cryptopp/main.cpp
@@ -1,21 +1,21 @@
-#include <sha.h>
-#include <hex.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/hex.h>
 #include <iostream>
 #include <string>
 
 int main() {
-    byte digest[ CryptoPP::SHA::DIGESTSIZE ];
+    CryptoPP::byte digest[ CryptoPP::SHA1::DIGESTSIZE ];
     std::string message = "abcdefghijklmnopqrstuvwxyz";
-    
-    CryptoPP::SHA hash;
-    hash.CalculateDigest( digest, (const byte*)message.c_str(), message.length() );
-    
+
+    CryptoPP::SHA1 hash;
+    hash.CalculateDigest( digest, (const CryptoPP::byte*)message.c_str(), message.length() );
+
     CryptoPP::HexEncoder encoder;
     std::string output;
-    
+
     encoder.Attach( new CryptoPP::StringSink( output ) );
     encoder.Put( digest, sizeof(digest) );
     encoder.MessageEnd();
-    
+
     std::cout << output << std::endl;
 }
