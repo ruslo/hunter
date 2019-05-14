c53a3a2f9f37260f9bc92b027d07cb9613bbfbe1
Paweł Bylica <chfast@gmail.com>
2019-05-14 18:22:03 +0200

    Update 'intx' to v0.2.0

--- cmake/projects/intx/hunter.cmake
+++ cmake/projects/intx/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     a70da498a9868e9484e01530c22690910d7222e6
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    intx
+    VERSION
+    0.2.0
+    URL
+    https://github.com/chfast/intx/archive/v0.2.0.tar.gz
+    SHA1
+    50c4d8ec601114755be9fd5bf5b288ee4eeacd54
+)
+
 hunter_cmake_args(
     intx
     CMAKE_ARGS
