2f7ee73407590ce93b34c3078596223fa516481a
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-05-14 22:08:56 +0300

    x264 snapshot-20190513-2245

--- cmake/projects/x264/hunter.cmake
+++ cmake/projects/x264/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     679b8c4f39f375090bf64e5c80c83303e4ddc37c
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    x264
+    VERSION
+    "snapshot-20190513-2245"
+    URL
+    "https://download.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20190513-2245-stable.tar.bz2"
+    SHA1
+    2e6faf305fb48a1449bdb61fe219b0c4d9501c36
+)
+
 hunter_configuration_types(x264 CONFIGURATION_TYPES Release)
 hunter_pick_scheme(DEFAULT url_sha1_x264)
 hunter_cmake_args(x264 CMAKE_ARGS PKGCONFIG_EXPORT_TARGETS=x264)
