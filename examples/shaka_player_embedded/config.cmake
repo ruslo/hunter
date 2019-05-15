a6068f6df54a15be36478c13755173e40d422570
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-05-15 23:55:28 +0300

    Use SHAKA_PLAYER_EMBEDDED_IOS_FRAMEWORK=OFF for testing

--- examples/shaka_player_embedded/config.cmake
+++ examples/shaka_player_embedded/config.cmake
@@ -10,3 +10,12 @@ hunter_config(
     VERSION 3.29.86-90da229-p0
 )
 # DOCUMENTATION_END }
+
+if(IOS)
+  # Test only compile/link, not real layout
+  hunter_config(
+      shaka_player_embedded
+      VERSION ${HUNTER_shaka_player_embedded_VERSION}
+      CMAKE_ARGS SHAKA_PLAYER_EMBEDDED_IOS_FRAMEWORK=OFF
+  )
+endif()
