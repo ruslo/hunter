0d19dbacdfce5bd21774499f9551cd2de56ad258
Krasimir Georgiev <krasimir.georgiev@seebyte.com>
2019-05-29 12:52:21 +0100

    Update 'ros_console_bridge' to 0.4.3-p0

--- cmake/projects/ros_console_bridge/hunter.cmake
+++ cmake/projects/ros_console_bridge/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     "0ed03b76b1289b5399b5dfdd4a4002596239a06f"
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    ros_console_bridge
+    VERSION
+    0.4.3-p0
+    URL
+    "https://github.com/hunter-packages/ros_console_bridge/archive/v0.4.3-p0.tar.gz"
+    SHA1
+    "9c1827626891522b626059d66cd3f32d9e99c931"
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(ros_console_bridge)
 hunter_download(PACKAGE_NAME ros_console_bridge)
