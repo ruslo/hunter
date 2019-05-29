5dcd916921c0ad1fb9bce2c8fa7e80f6ae38294a
caseymcc <caseymcc@krazer.net>
2019-05-29 16:48:21 -0500

    Update imgui package to 1.70

--- cmake/projects/imgui/hunter.cmake
+++ cmake/projects/imgui/hunter.cmake
@@ -12,9 +12,20 @@ hunter_add_version(
     PACKAGE_NAME
     imgui
     VERSION
+    "1.70"
+    URL
+    "https://github.com/hunter-packages/imgui/archive/v1.70.p0.tar.gz"
+    SHA1
+    36a806ecc3b43204e4a895d6842f88519d3d4b98
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    imgui
+    VERSION
     "1.62"
     URL
-   "https://github.com/hunter-packages/imgui/archive/v1.62.p0.tar.gz"
+    "https://github.com/hunter-packages/imgui/archive/v1.62.p0.tar.gz"
     SHA1
     0e1fb683036a45372bc34d39c29068443b6d97e6
 )
@@ -23,6 +34,7 @@ hunter_cmake_args(
     imgui
     CMAKE_ARGS
         IMGUI_BUILD_EXAMPLES=OFF
+        IMGUI_ENABLE_TEST_ENGINE=OFF
 )
 
 # Pick a download scheme
