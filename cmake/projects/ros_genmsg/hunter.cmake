0eacb2c4e1ae6704672b92b78e095b55905ee441
Krasimir Georgiev <kgeorgiev1993@gmail.com>
2019-05-31 01:05:32 +0100

    Update 'ros_genmsg' to 0.5.12-p0

--- cmake/projects/ros_genmsg/hunter.cmake
+++ cmake/projects/ros_genmsg/hunter.cmake
@@ -20,6 +20,17 @@ hunter_add_version(
     "3003f752c6b9d5a6c5771dd1448b61011cfcc63d"
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    ros_genmsg
+    VERSION
+    0.5.12-p0
+    URL
+    "https://github.com/hunter-packages/ros_genmsg/archive/v0.5.12-p0.tar.gz"
+    SHA1
+    "e4009ea247bfb3683700f0e3097fc1b9a884a378"
+)
+
 hunter_cmake_args(
     ros_genmsg
     CMAKE_ARGS
