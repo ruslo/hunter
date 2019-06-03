2717d5c5a93076a837eca8d7eb344b56cc29a44a
Krasimir Georgiev <krasimir.georgiev@seebyte.com>
2019-06-03 13:38:58 +0100

    Update 'ros_std_msgs' to 0.5.12-p0

--- cmake/projects/ros_std_msgs/hunter.cmake
+++ cmake/projects/ros_std_msgs/hunter.cmake
@@ -20,6 +20,17 @@ hunter_add_version(
     "04ad9f0e155b27870c7ad2293ace496523f2e960"
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    ros_std_msgs
+    VERSION
+    0.5.12-p0
+    URL
+    "https://github.com/hunter-packages/ros_std_msgs/archive/v0.5.12-p0.tar.gz"
+    SHA1
+    "68f90e7d9611da0d18f3aab520d76b03e685e5a0"
+)
+
 hunter_cmake_args(
     ros_std_msgs
     CMAKE_ARGS
