80075383d498a3bf2050405dd25be80e5dc9a31f
Krasimir Georgiev <krasimir.georgiev@seebyte.com>
2019-06-04 11:46:36 +0100

    Update 'ros' to 1.14.6-p0

--- cmake/projects/ros/hunter.cmake
+++ cmake/projects/ros/hunter.cmake
@@ -20,6 +20,17 @@ hunter_add_version(
     "9ca792b460313dbe161760713abb24dd28d3b35d"
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    ros
+    VERSION
+    1.14.6-p0
+    URL
+    "https://github.com/hunter-packages/ros/archive/v1.14.6-p0.tar.gz"
+    SHA1
+    "fb44ebcebcc17664ddd6324366beee9ee90d2a2d"
+)
+
 hunter_cmake_args(
     ros_message_generation
     CMAKE_ARGS
