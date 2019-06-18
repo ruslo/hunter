90e7b50311985834c3f1475e672e620b7d0e1382
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-06-19 00:58:24 +0300

    mshadow 1.1-1d79ecf-p0

--- cmake/projects/mshadow/hunter.cmake
+++ cmake/projects/mshadow/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     34429211cc841afbde7f8f36994501836a36eb6f
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    mshadow
+    VERSION
+    1.1-1d79ecf-p0
+    URL
+    "https://github.com/hunter-packages/mshadow/archive/v1.1-1d79ecf-p0.tar.gz"
+    SHA1
+    45e424dc081ccd02f61ab58b59030e3da3890a8a
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(mshadow)
 hunter_download(PACKAGE_NAME mshadow)
