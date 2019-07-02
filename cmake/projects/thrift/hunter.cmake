d07a9613b71c76529a335bb8b4f442d3a308ab69
JochenGuck <42436701+JochenGuck@users.noreply.github.com>
2019-07-02 11:45:43 +0200

    Update 'thrift' to v0.12.0-p0 (#1908)

--- cmake/projects/thrift/hunter.cmake
+++ cmake/projects/thrift/hunter.cmake
@@ -8,6 +8,12 @@ include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME thrift
+    VERSION "0.12.0-p0"
+    URL "https://github.com/hunter-packages/thrift/archive/v0.12.0-p0.tar.gz"
+    SHA1 "dafb78da4a6f711819a1962c883b64272a34364c")
+
+hunter_add_version(
+    PACKAGE_NAME thrift
     VERSION "0.11.0-p0"
     URL "https://github.com/hunter-packages/thrift/archive/v0.11.0-p0.tar.gz"
     SHA1 "1dff5a2a26de3bd9d4bc8c68db09215c95874001")
