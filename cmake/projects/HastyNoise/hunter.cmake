b8c40558f768ea25044b6c04120b7b39a4d0e238
caseymcc <caseymcc@krazer.net>
2019-07-10 13:13:58 -0500

    update to HastyNoise 0.8.2

--- cmake/projects/HastyNoise/hunter.cmake
+++ cmake/projects/HastyNoise/hunter.cmake
@@ -12,6 +12,17 @@ hunter_add_version(
     PACKAGE_NAME
     HastyNoise
     VERSION
+    "0.8.2"
+    URL
+   "https://github.com/caseymcc/HastyNoise/archive/0.8.2.tar.gz"
+    SHA1
+    563bd6d8e5cd7fd37ec4a52fd4c12e7bf722685b
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    HastyNoise
+    VERSION
     "0.8.1"
     URL
    "https://github.com/caseymcc/HastyNoise/archive/0.8.1.tar.gz"
@@ -28,6 +39,12 @@ hunter_add_version(
    "https://github.com/caseymcc/HastyNoise/archive/0.8.0.tar.gz"
     SHA1
     f4e9fd3fb2b53eeb10d6c7705d34eac7c7493059
+)
+
+hunter_cmake_args(
+    HastyNoise
+    CMAKE_ARGS
+        HN_PREVIEW_APP=OFF
 )
 
 # Pick a download scheme
