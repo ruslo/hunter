9e80ca48fca0140d3936e4eb1e12c763e7caebb2
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-09-11 10:11:29 +0300

    s3 0.0.0-287e4be-p1

--- cmake/projects/s3/hunter.cmake
+++ cmake/projects/s3/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     da8fae10bcda3126b0847b2a255e62435afc313e
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    s3
+    VERSION
+    0.0.0-287e4be-p1
+    URL
+    "https://github.com/hunter-packages/libs3/archive/v0.0.0-287e4be-p1.tar.gz"
+    SHA1
+    908d859fcde3424ee5e3226288d2604c2419d996
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(s3)
 hunter_download(PACKAGE_NAME s3)
