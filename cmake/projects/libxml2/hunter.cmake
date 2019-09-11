7a12cb41f0314a6c78c83caf05449a39fcca7c41
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-09-11 09:40:44 +0300

    libxml2 2.9.7-p0

--- cmake/projects/libxml2/hunter.cmake
+++ cmake/projects/libxml2/hunter.cmake
@@ -4,71 +4,21 @@
 # !!! DO NOT PLACE HEADER GUARDS HERE !!!
 
 include(hunter_add_version)
-include(hunter_cmake_args)
 include(hunter_cacheable)
-include(hunter_configuration_types)
-include(hunter_pick_scheme)
 include(hunter_download)
-include(hunter_check_toolchain_definition)
+include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME
     libxml2
     VERSION
-    "2.9.7"
+    "2.9.7-p0"
     URL
-    "http://xmlsoft.org/sources/libxml2-2.9.7.tar.gz"
+    "https://github.com/hunter-packages/libxml2/archive/v2.9.7-p0.tar.gz"
     SHA1
-    ab3325e6cdda50ab2382fdfe0bdb6f7d1b9224a6
+    5d5d6da2a87267f160f76a26c5637cbc48b28784
 )
 
-hunter_add_version(
-    PACKAGE_NAME
-    libxml2
-    VERSION
-    "2.9.4"
-    URL
-    "http://xmlsoft.org/sources/libxml2-2.9.4.tar.gz"
-    SHA1
-    958ae70baf186263a4bd801a81dd5d682aedd1db
-)
-
-set(
-    _libxml_unrelocatable_text_files
-    PACKAGE_UNRELOCATABLE_TEXT_FILES
-    "lib/pkgconfig/libxml-2.0.pc"
-    "lib/libxml2.la"
-    "bin/xml2-config"
-    "lib/xml2Conf.sh"
-)
-
-hunter_configuration_types(libxml2 CONFIGURATION_TYPES Release)
-
-if (MSVC)
-    hunter_check_toolchain_definition(NAME "_DLL" DEFINED _hunter_vs_md)
-    hunter_pick_scheme(DEFAULT url_sha1_libxml2_msvc)
-    # pkgconfig file not applicable
-    set(_libxml_unrelocatable_text_files "")
-    hunter_cmake_args(
-        libxml2
-            CMAKE_ARGS
-                LIBXML2_BUILD_DYNAMIC_VSRUNTIME=${_hunter_vs_md}
-    )
-else()
-    hunter_pick_scheme(DEFAULT url_sha1_autotools)
-    # Drop dependencies
-    hunter_cmake_args(
-        libxml2
-        CMAKE_ARGS
-        EXTRA_FLAGS=--without-python --without-lzma --without-zlib --without-iconv
-        PKGCONFIG_EXPORT_TARGETS=libxml-2.0
-    )
-endif()
-
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(libxml2)
-
-hunter_download(
-    PACKAGE_NAME libxml2
-    PACKAGE_INTERNAL_DEPS_ID "11"
-    ${_libxml_unrelocatable_text_files}
-)
+hunter_download(PACKAGE_NAME libxml2)
